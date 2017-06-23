fe92e420ac27fb072aac67458dc582da35c4b31c
Thibaut Boustany <thibaut@ledger.fr>
2017-06-23 10:49:21 +0200

    Menu entries + sassification

--- src/Menu.js
+++ src/Menu.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import Chip from 'material-ui/Chip';
 
 import './Menu.css';
 
 function Menu() {
   return (
     <div className="Menu">
-      &nbsp;
+      <ul className="main-menu">
+        <li><a href="/"><i className="material-icons">home</i> Dashboard</a></li>
+        <li><a href="/new"><i className="material-icons">add</i> New operation</a></li>
+        <li><a href="/pending"><i className="material-icons">format_align_left</i> Pending requests</a> <span className="menu-badge">2</span></li>
+        <li><a href="/search"><i className="material-icons">search</i> Search</a></li>
+      </ul>
     </div>
   );
 }
