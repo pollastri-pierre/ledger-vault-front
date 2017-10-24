628a0a9c975a3d41d7f76b7153454202275a2a2c
Gaëtan Renaudeau <gaetan.renaudeau@ledger.fr>
2017-10-24 13:58:14 +0200

    fix CurrencyNameValue

--- src/components/CurrencyNameValue.js
+++ src/components/CurrencyNameValue.js
@@ -7,12 +7,6 @@ import currencies from '../currencies';
 // This is a "smart" component that accepts a currencyName (e.g. bitcoin) and a value number
 // and infer the proper "unit" to use and delegate to CurrencyUnitValue
 
-// TODO:
-// we need to fetch all the available currencies. I suggest this is statically loaded, maybe injected from a server currencies.js script
-// we also need to have a lib for world currencies.
-// this will depend on user pref (if you select mBTC vs BTC , etc..)
-// we might have a redux store that store user prefered unit per currencyName
-// we also need to lookup the world currencies (eur, usd, ...)
 class CurrencyNameValue extends PureComponent {
   props: {
     currencyName: number,
@@ -27,7 +21,13 @@ class CurrencyNameValue extends PureComponent {
       unit = counterValueUnits[currencyName];
     } else {
       // try to find a crypto currencies unit
-      unit = currencies.find(c => c.name === currencyName);
+      const currency = currencies.find(c => c.name === currencyName);
+      if (currency) {
+        // TODO:
+        // this will depend on user pref (if you select mBTC vs BTC , etc..)
+        // we might have a redux store that store user prefered unit per currencyName
+        unit = currency.units[0];
+      }
     }
 
     if (!unit) {
