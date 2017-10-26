5794cdeb8385bc1b7d043d43b86a8afb9ac1e8f7
Thibaut Boustany <thibaut@ledger.fr>
2017-10-26 15:30:00 +0200

    Fix currencies formatting

--- public/all.currencies.js
+++ public/all.currencies.js
@@ -1,87 +1,86 @@
 window.CRYPTO_CURRENCIES = [
   {
     name: "bitcoin",
-    family: 'bitcoin',
-    color: '#fcb653',
+    family: "BITCOIN",
+    color: "#fcb653",
     units: [
       {
-        name: 'bitcoin',
-        code: 'BTC',
-        symbol: 'Ƀ',
-        magnitude: 8,
+        name: "bitcoin",
+        code: "BTC",
+        symbol: "Ƀ",
+        magnitude: 8
       },
       {
-        name: 'millibitcoin',
-        code: 'mBTC',
-        symbol: 'mBTC',
-        magnitude: 5,
-      },
-    ],
+        name: "millibitcoin",
+        code: "mBTC",
+        symbol: "mBTC",
+        magnitude: 5
+      }
+    ]
   },
   {
-    name: 'dogecoin',
-    family: 'bitcoin',
-    color: '#65d196',
+    name: "dogecoin",
+    family: "BITCOIN",
+    color: "#65d196",
     units: [
       {
-        name: 'dogecoin',
-        code: 'DOGE',
-        symbol: 'DOGE',
-        magnitude: 8,
-      },
-    ],
+        name: "dogecoin",
+        code: "DOGE",
+        symbol: "DOGE",
+        magnitude: 8
+      }
+    ]
   },
   {
-    name: 'dash',
-    family: 'bitcoin',
-    color: '#0e76aa',
+    name: "dash",
+    family: "BITCOIN",
+    color: "#0e76aa",
     units: [
       {
-        name: 'dash',
-        code: 'DASH',
-        symbol: 'DASH',
-        magnitude: 8,
-      },
-    ],
+        name: "dash",
+        code: "DASH",
+        symbol: "DASH",
+        magnitude: 8
+      }
+    ]
   },
   {
-    name: 'ethereum',
-    family: 'ethereum',
-    color: '#27d0e2',
+    name: "ethereum",
+    family: "ETHEREUM",
+    color: "#27d0e2",
     units: [
       {
-        name: 'ether',
-        code: 'ETH',
-        symbol: 'ETH',
-        magnitude: 8,
-      },
-    ],
+        name: "ether",
+        code: "ETH",
+        symbol: "ETH",
+        magnitude: 8
+      }
+    ]
   },
   {
-    name: 'ethereum-classic',
-    family: 'ethereum',
-    color: '#3ca569',
+    name: "ethereum-classic",
+    family: "ETHEREUM",
+    color: "#3ca569",
     units: [
       {
-        name: 'ether',
-        code: 'ETC',
-        symbol: 'ETC',
-        magnitude: 8,
-      },
-    ],
+        name: "ether",
+        code: "ETC",
+        symbol: "ETC",
+        magnitude: 8
+      }
+    ]
   },
   {
-
-    name: 'litecoin',
-    family: 'bitcoin',
-    color: '#cccccc',
+    name: "litecoin",
+    family: "BITCOIN",
+    color: "#cccccc",
     units: [
       {
-        name: 'litecoin',
-        code: 'LTC',
-        symbol: 'LTC',
-        magnitude: 8,
-      },
-    ],
-  },
+        name: "litecoin",
+        code: "LTC",
+        symbol: "LTC",
+        magnitude: 8
+      }
+    ]
+  }
 ];
