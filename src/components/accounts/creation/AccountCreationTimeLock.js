2c6fb3ad0324c99a0bc8f3313d865f06c788a76b
Florent Teissier <florentteissier@MacFlo.local>
2017-11-20 15:04:17 +0100

    RateLimiter TimeLock should have their form disabled and greyed if not enabled

--- src/components/accounts/creation/AccountCreationTimeLock.js
+++ src/components/accounts/creation/AccountCreationTimeLock.js
@@ -2,6 +2,7 @@
 import React from "react";
 import Checkbox from "../../form/Checkbox";
 import { PopBubble, DialogButton } from "../../";
+import EnableForm from "../../../components/EnableForm";
 import ArrowDown from "../../icons/ArrowDown";
 
 function AccountCreationTimeLock(props: {
@@ -31,90 +32,88 @@ function AccountCreationTimeLock(props: {
         <h3>Time-lock</h3>
       </header>
       <div className="content">
-        <div
-          className="form-field-checkbox"
-          onClick={enable}
-          role="button"
-          tabIndex={0}
-        >
-          <label htmlFor="enable-timelock">Enable</label>
-          <Checkbox
-            checked={timelock.enabled}
-            handleInputChange={enable}
-            labelFor="enable-timelock"
-          />
-        </div>
-        <div className="form-field">
-          <input
-            className="small-padding"
-            type="text"
-            id="text-duration"
-            value={timelock.value}
-            onChange={e => change(e.target.value)}
-          />
-          <label htmlFor="text-duration">Duration</label>
-          <span
-            className="count dropdown"
-            role="button"
-            tabIndex={0}
-            onClick={e => openPopBubble(e.currentTarget)}
-          >
-            {timelock.frequency.label}
-            <ArrowDown className="arrow-down" />
-          </span>
-          <PopBubble
-            open={popbubble}
-            onRequestClose={openPopBubble}
-            anchorEl={anchor}
-            style={{
-              marginLeft: "34px",
-              marginTop: "11px"
-            }}
-          >
-            <div className="frequency-bubble">
-              <div
-                role="button"
-                tabIndex={0}
-                onClick={() =>
-                  changeFrequency("timelock", { label: "minutes", value: 60 })}
-                className={`frequency-bubble-row ${timelock.frequency.label ===
-                "minutes"
-                  ? "active"
-                  : ""}`}
-              >
-                minutes
+        <EnableForm checked={timelock.enabled} toggle={enable}>
+          <div className="form-field">
+            <input
+              className="small-padding"
+              type="text"
+              id="text-duration"
+              value={timelock.value}
+              onChange={e => change(e.target.value)}
+            />
+            <label htmlFor="text-duration">Duration</label>
+            <span
+              className="count dropdown"
+              role="button"
+              tabIndex={0}
+              onClick={e => openPopBubble(e.currentTarget)}
+            >
+              {timelock.frequency.label}
+              <ArrowDown className="arrow-down" />
+            </span>
+            <PopBubble
+              open={popbubble}
+              onRequestClose={openPopBubble}
+              anchorEl={anchor}
+              style={{
+                marginLeft: "34px",
+                marginTop: "11px"
+              }}
+            >
+              <div className="frequency-bubble">
+                <div
+                  role="button"
+                  tabIndex={0}
+                  onClick={() =>
+                    changeFrequency("timelock", {
+                      label: "minutes",
+                      value: 60
+                    })}
+                  className={`frequency-bubble-row ${timelock.frequency
+                    .label === "minutes"
+                    ? "active"
+                    : ""}`}
+                >
+                  minutes
+                </div>
+                <div
+                  role="button"
+                  tabIndex={0}
+                  onClick={() =>
+                    changeFrequency("timelock", {
+                      label: "hours",
+                      value: 3600
+                    })}
+                  className={`frequency-bubble-row ${timelock.frequency
+                    .label === "hours"
+                    ? "active"
+                    : ""}`}
+                >
+                  hours
+                </div>
+                <div
+                  role="button"
+                  tabIndex={0}
+                  onClick={() =>
+                    changeFrequency("timelock", {
+                      label: "days",
+                      value: 84600
+                    })}
+                  className={`frequency-bubble-row ${timelock.frequency
+                    .label === "days"
+                    ? "active"
+                    : ""}`}
+                >
+                  days
+                </div>
               </div>
-              <div
-                role="button"
-                tabIndex={0}
-                onClick={() =>
-                  changeFrequency("timelock", { label: "hours", value: 3600 })}
-                className={`frequency-bubble-row ${timelock.frequency.label ===
-                "hours"
-                  ? "active"
-                  : ""}`}
-              >
-                hours
-              </div>
-              <div
-                role="button"
-                tabIndex={0}
-                onClick={() =>
-                  changeFrequency("timelock", { label: "days", value: 84600 })}
-                className={`frequency-bubble-row ${timelock.frequency.label ===
-                "days"
-                  ? "active"
-                  : ""}`}
-              >
-                days
-              </div>
-            </div>
-          </PopBubble>
-        </div>
-        <p className="info">
-          Time-lock delays each outgoing operation by a configurable length,
-          after all the required members have given their approvals.
-        </p>
+            </PopBubble>
+          </div>
+          <p className="info">
+            Time-lock delays each outgoing operation by a configurable length,
+            after all the required members have given their approvals.
+          </p>
+        </EnableForm>
       </div>
 
       <div className="footer">
