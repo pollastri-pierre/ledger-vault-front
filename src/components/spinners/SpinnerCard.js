3ee6a8f9f3795d809a41cdd39024e8af73d02884
Gaëtan Renaudeau <gaetan.renaudeau@ledger.fr>
2017-11-27 19:58:31 +0100

    SpinnerCard will also display the global loader

--- src/components/spinners/SpinnerCard.js
+++ src/components/spinners/SpinnerCard.js
@@ -1,20 +1,30 @@
 // @flow
-import React from "react";
+import React, { PureComponent } from "react";
 import CircularProgress from "material-ui/CircularProgress";
+import { load } from "../GlobalLoading";
 
-const SpinnerCard = () => {
-  return (
-    <CircularProgress
-      size={30}
-      style={{
-        position: "absolute",
-        top: "50%",
-        left: "50%",
-        marginLeft: "-15px",
-        marginTop: "-15px"
-      }}
-    />
-  );
-};
+class SpinnerCard extends PureComponent<{}> {
+  unload: Function;
+  componentDidMount() {
+    this.unload = load();
+  }
+  componentWillUnmount() {
+    this.unload();
+  }
+  render() {
+    return (
+      <CircularProgress
+        size={30}
+        style={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          marginLeft: "-15px",
+          marginTop: "-15px"
+        }}
+      />
+    );
+  }
+}
 
 export default SpinnerCard;
