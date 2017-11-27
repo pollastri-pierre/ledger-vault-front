13c7fd7b85864e1377fd93f0d2580becc66de510
Thibaut Boustany <thibaut@ledger.fr>
2017-11-27 16:47:50 +0100

    Fix counter values on operation creation details

--- src/components/operations/creation/OperationCreationAccounts.js
+++ src/components/operations/creation/OperationCreationAccounts.js
@@ -2,6 +2,8 @@ import _ from "lodash";
 import PropTypes from "prop-types";
 import React from "react";
 import CurrencyNameValue from "../../CurrencyNameValue";
+import CurrencyUnitValue from "../../CurrencyUnitValue";
+import { countervalueForRate } from "../../../data/currency";
 
 import "./OperationCreationAccounts.css";
 
@@ -11,36 +13,45 @@ function OperationCreationAccounts(props) {
   return (
     <div className="operation-creation-accounts wrapper">
       <div className="tab-title">Account to debit</div>
-      {_.map(accounts, cur => (
-        <div
-          onClick={() => onSelect(cur)}
-          role="button"
-          tabIndex="0"
-          key={cur.id}
-          className={`operation-creation-account
+      {_.map(accounts, cur => {
+        const counterValueUnit = countervalueForRate(
+          cur.currencyRate,
+          cur.balance
+        );
+
+        return (
+          <div
+            onClick={() => onSelect(cur)}
+            role="button"
+            tabIndex="0"
+            key={cur.id}
+            className={`operation-creation-account
             ${cur.currency.name
               .split(" ")
               .join("-")
               .toLowerCase()}
-            ${selectedAccount && selectedAccount.id === cur.id
-              ? "selected"
-              : ""}`}
-        >
-          <div className="account-top">
-            <span className="account-name">{cur.name}</span>
-            <span className="account-balance">
-              <CurrencyNameValue
-                currencyName={cur.currency.name}
-                value={cur.balance}
-              />
-            </span>
+            ${
+              selectedAccount && selectedAccount.id === cur.id ? "selected" : ""
+            }`}
+          >
+            <div className="account-top">
+              <span className="account-name">{cur.name}</span>
+              <span className="account-balance">
+                <CurrencyNameValue
+                  currencyName={cur.currency.name}
+                  value={cur.balance}
+                />
+              </span>
+            </div>
+            <div className="account-botom">
+              <span className="account-currency">{cur.currency.name}</span>
+              <span className="account-countervalue">
+                <CurrencyUnitValue {...counterValueUnit} />
+              </span>
+            </div>
           </div>
-          <div className="account-botom">
-            <span className="account-currency">{cur.currency.name}</span>
-            <span className="account-countervalue">EUR 42</span>
-          </div>
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 }
