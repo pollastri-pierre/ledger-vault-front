b710118565cf9aba179b747c72b3a4c42da123b8
Malik Fassi Fihri <malikfassifihri@gmail.com>
2017-12-07 15:22:40 +0100

    fix some typo & wording, WIP integration of  Auto expire securitySchema

--- src/components/SettingsModal/index.js
+++ src/components/SettingsModal/index.js
@@ -17,7 +17,8 @@ import DialogButton from "../buttons/DialogButton";
 import {
   BigSecurityTimeLockIcon,
   BigSecurityMembersIcon,
-  BigSecurityRateLimiterIcon
+  BigSecurityRateLimiterIcon,
+  BigSecurityAutoExpireIcon
 } from "../icons";
 import BadgeSecurity from "../BadgeSecurity";
 import RateLimiterValue from "../RateLimiterValue";
@@ -38,8 +39,13 @@ class NavAccount extends Component<{
 }> {
   render() {
     const { account } = this.props;
+    console.log(account);
     return (
-      <NavLink className="nav-account" to={account.id}>
+      <NavLink
+        className="nav-account"
+        style={{ color: account.currency.color }}
+        to={account.id}
+      >
         <span className="name">{account.name}</span>
         <span className="currency">{account.currency.name}</span>
       </NavLink>
@@ -67,7 +73,13 @@ class SecuritySchemeView extends Component<{
 }> {
   render() {
     const {
-      securityScheme: { quorum, approvers, time_lock, rate_limiter }
+      securityScheme: {
+        quorum,
+        approvers,
+        time_lock,
+        rate_limiter,
+        auto_expire
+      }
     } = this.props;
     return (
       <div className="security-scheme">
@@ -94,6 +106,12 @@ class SecuritySchemeView extends Component<{
               />
             )
           }
+        />
+        <BadgeSecurity
+          icon={<BigSecurityAutoExpireIcon />}
+          label="Auto-expire"
+          disabled={!auto_expire}
+          value={<TimeLockValue time_lock={auto_expire} />}
         />
       </div>
     );
@@ -226,7 +244,7 @@ class AccountSettingsEdit extends Component<Props, State> {
             </Select>
           </SettingsField>
           {countervalueSourceData ? (
-            <SettingsField label="Source">
+            <SettingsField label="Fiat Currency">
               <Select onChange={this.onFiatChange} theme="black">
                 {countervalueSourceData.fiats.map(fiat => (
                   <Option
