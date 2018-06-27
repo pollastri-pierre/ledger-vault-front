92f1d132c0323ed4bcc61406d10b98af74a50d6e
Florent Teissier <teissierflorent@gmail.com>
2018-06-27 17:03:29 +0200

    fix dynamically get location path org name

--- src/network/fetchF.js
+++ src/network/fetchF.js
@@ -8,12 +8,14 @@ if (process.env.NODE_ENV === "test") {
   const port = "5000";
   const baseUrl =
     process.env.NODE_ENV === "development"
-      ? `https://localhost`
+      ? `https://localhost:${port}`
       : `https://beta.vault.ledger.fr`;
   fetchF = (uri: string, options: Object): Promise<*> => {
     let prefix = location.pathname.split("/")[1];
-    if (prefix !== "") {
+    if (prefix !== "" && process.env.NODE_ENV !== "development") {
       prefix = "/gate/" + prefix;
+    } else {
+      prefix = "/" + prefix;
     }
     return fetch(baseUrl + prefix + uri, options);
   };
