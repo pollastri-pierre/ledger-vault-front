62ab92a19f6aafb00c3fec5b1f6adde550f894da
Florent Teissier <teissierflorent@gmail.com>
2018-08-08 11:35:58 +0200

    change input parameter public_key to pub_key in approve endpoints

--- src/api/mutations/ApproveOperationMutation.js
+++ src/api/mutations/ApproveOperationMutation.js
@@ -27,6 +27,6 @@ export default class ApproveOperationMutation extends Mutation<In, Res> {
 
   getBody() {
     const { public_key, approval } = this.props;
-    return { public_key, approval };
+    return { pub_key: public_key, approval };
   }
 }
