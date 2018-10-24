c751c31bdfc24ae88900af2c63de768cd3ddd752
Najla <najla.amdouni@ledger.fr>
2018-10-24 18:14:55 +0200

    abort account and operation test cases done

--- cypress/integration/06_account_abort.spec.js
+++ cypress/integration/06_account_abort.spec.js
@@ -18,8 +18,10 @@ context("Account Abort", () => {
     });
     cy.server();
     cy.route("post", "**/authentications/**").as("authenticate");
-    cy.route("post", "**/abort").as("abort");
+    cy.route("post", "**/approve").as("approve");
     cy.route("post", "**/logout").as("logout");
+    cy.route("post", "**/abort").as("abort");
+    cy.route("get", "**/pending").as("pending");
 
     cy
       .request("POST", Cypress.env('api_switch_device'), {
@@ -58,8 +60,6 @@ context("Account Abort", () => {
           cy.contains("done").click();
           // go to confirmation tab
           cy.contains("continue").click();
-
-          // click on done to create the account, it will display the authenticate with device modal
           cy.contains("done").click();
           cy.wait("@authenticate");
           // Abort the account
@@ -68,8 +68,6 @@ context("Account Abort", () => {
             .get(".test-pending-account")
             .eq(0)
             .click();
-
-          // // click 2 times on abort to abort
           cy
             .get("button")
             .contains("Abort")
@@ -84,7 +82,6 @@ context("Account Abort", () => {
             .get(".top-message-title")
             .contains("account request aborted");
           cy.wait("@abort");
-
           // After logout we should get a message
           cy.contains("view profile").click();
           cy.contains("logout").click();
@@ -95,5 +92,133 @@ context("Account Abort", () => {
             .get(".top-message-title")
             .contains("See you soon!");
         });
+        cy
+          .request("POST", "http://localhost:5001/switch-device", {
+            device_number: 6
+          })
+          .then(() => {
+            cy.get("input").type(orga_name);
+            cy.contains("continue").click();
+            cy.wait("@authenticate");
+            // Acount Litcoin
+            cy.get(".test-new-account").click();
+            cy.contains("Litecoin").click();
+            cy.get("input").type("LTC TEST");
+            cy.contains("continue").click();
+            cy.contains("Members").click();
+            // select 2 members
+            cy
+              .get(".test-member-row")
+              .eq(0)
+              .click({ force: true });
+            cy
+              .get(".test-member-row")
+              .eq(1)
+              .click({ force: true });
+              cy.contains("Done").click();
+              cy.contains("Approvals").click();
+              cy.get("input").clear();
+              cy.get("input").type(1);
+              cy.contains("done").click();
+              // go to confirmation tab
+              cy.contains("continue").click();
+              // click on done to create the account, it will display the authenticate with device modal
+              cy.contains("done").click();
+              cy.wait("@authenticate");
+              cy.contains("Pending").click();
+
+              cy
+                .get('.test-pending-account')
+                .eq(0)
+                .click();
+              cy.wait("@pending");
+              cy
+                .get("button")
+                .contains("Approve")
+                .click();
+              cy.wait("@approve");
+              cy.contains("view profile").click();
+              cy.contains("logout").click();
+              cy.wait("@logout");
+              cy
+                .request("POST", "http://localhost:5001/switch-device", {
+                  device_number: 5
+                })
+              cy.get("input").type(orga_name);
+              cy.contains("continue").click();
+              cy.wait("@authenticate");
+              cy.contains("Pending").click();
+              cy
+                .get('.test-pending-account')
+                .eq(0)
+                .click();
+              cy.wait("@pending");
+              cy
+                .get("button")
+                .contains("Abort")
+                .click({ force: true });
+              cy
+                .get("button")
+                .contains("Abort")
+                .click({ force: true });
+              cy
+                .get(".top-message-body")
+                .contains("the account request has been successfully aborted")
+                .get(".top-message-title")
+                .contains("account request aborted");
+              cy.wait("@abort");
+
+              // Acount Dash
+              cy.get(".test-new-account").click();
+              cy.contains("Dash").click();
+              cy.get("input").type("DASH TEST");
+              cy.contains("continue").click();
+              // open the list of members modal
+              cy.contains("Members").click();
+              // select 2 members
+              cy
+                .get(".test-member-row")
+                .eq(0)
+                .click({ force: true });
+              cy
+                .get(".test-member-row")
+                .eq(1)
+                .click({ force: true });
+                cy.contains("Done").click();
+                cy.contains("None").click();
+                cy.get("input").clear();
+                cy.get("input").type(1);
+                cy.contains("done").click();
+                // go to confirmation tab
+                cy.contains("continue").click();
+
+                // click on done to create the account, it will display the authenticate with device modal
+                cy.contains("done").click();
+                cy.wait("@authenticate");
+                cy.contains("Pending").click();
+                cy
+                  .get('.test-pending-account')
+                  .eq(0)
+                  .click();
+                cy.wait("@pending");
+                cy
+                  .get("button")
+                  .contains("Abort")
+                  .click();
+                cy
+                  .get("button")
+                  .contains("Abort")
+                  .click();
+                cy
+                  .get(".top-message-body")
+                  .contains("the account request has been successfully aborted")
+                  .get(".top-message-title")
+                  .contains("account request aborted");
+                cy.wait("@abort");
+                  //Logout
+                  cy.contains("view profile").click();
+                  cy.contains("logout").click();
+                  cy.wait("@logout");
+                });
     });
 });
---
+++ cypress/integration/08_account_dashboard.spec.js
@@ -0,0 +1,46 @@
+const orga_name = Cypress.env("workspace");
+context("Account Abort", () => {
+  let polyfill;
+  before(() => {
+    const polyfillUrl = Cypress.env('polyfillUrl');
+    cy.request(polyfillUrl).then(response => {
+      polyfill = response.body;
+    });
+  });
+  it("should fail with wrong device", () => {
+    cy.visit(Cypress.env('api_server'), {
+      onBeforeLoad: win => {
+        win.fetch = null;
+        win.eval(polyfill);
+        win.fetch = win.unfetch;
+      }
+    });
+    cy.server();
+    cy.route("post", "**/authentications/**").as("authenticate");
+    cy.route("post", "**/abort").as("abort");
+    cy.route("post", "**/logout").as("logout");
+    cy.route("get", "**/dashboard").as("dashboard");
+
+    cy
+      .request("POST", Cypress.env('api_switch_device'), {
+        device_number: 5
+      })
+      // Login with a wrong workspace name
+      .then(() => {
+        cy.get("input").type(orga_name);
+        cy.contains("continue").click();
+        cy.wait("@authenticate");
+
+        cy.contains("dashboard").click();
+        cy.contains("Total Balance").should('be.visible');
+        cy.contains("Last operations").should('be.visible');
+        cy.contains("currencies").should('be.visible');
+        cy.contains("pending").should('be.visible');
+
+        //Logout
+        cy.contains("view profile").click();
+        cy.contains("logout").click();
+        cy.wait("@logout");
+        });
+    });
+});
