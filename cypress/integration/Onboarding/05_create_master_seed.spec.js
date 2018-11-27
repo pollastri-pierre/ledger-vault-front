3aa222e31b6e403230c74a00cbc84d08cdb6e674
Najla <najla.amdouni@ledger.fr>
2018-11-27 16:25:17 +0100

    remove negative test case that will be on Wipe

--- cypress/integration/Onboarding/05_create_master_seed.spec.js
+++ cypress/integration/Onboarding/05_create_master_seed.spec.js
@@ -48,37 +48,12 @@ context("Create the Master Seed", () => {
         cy.get(":nth-child(1) > .fragment").click();
         cy.wait("@authenticate");
 
-
-        // Try to sign with wrong device should fail
-        cy.request("POST", Cypress.env("api_switch_device"), {
-            device_number: 1
-        });
-        cy.get(":nth-child(2) > .fragment").click();
-        cy
-          .get(".top-message-body")
-          .contains(
-            "Please connect a Shared-Owner device"
-          )
-          .get(".top-message-title")
-          .contains("Error");
-
         // Get Seed 2nd Shared Owner
         cy.request("POST", Cypress.env("api_switch_device"), {
           device_number: 8
         });
         cy.get(":nth-child(2) > .fragment").click();
         cy.wait("@authenticate");
-
-
-        // Try to see with the same device
-        cy.get(":nth-child(3) > .fragment").click();
-        cy
-          .get(".top-message-body")
-          .contains(
-            "This device has already been used to generate the master seed, please connect another one."
-          )
-          .get(".top-message-title")
-          .contains("Error");
 
         // Get Seed 3rd Shared Owner
         cy.request("POST", Cypress.env("api_switch_device"), {
