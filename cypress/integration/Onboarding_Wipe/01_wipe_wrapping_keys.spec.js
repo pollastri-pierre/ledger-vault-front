dc1a342333922895994ce53c0583031f01bf15a9
Najla <najla.amdouni@ledger.fr>
2018-11-27 18:40:30 +0100

    wipe onboarding tested

---
+++ cypress/integration/Onboarding_Wipe/01_wipe_wrapping_keys.spec.js
@@ -0,0 +1,90 @@
+const orga_name = Cypress.env("workspace");
+context("Create Wrapping Key", () => {
+  let polyfill;
+  before(() => {
+    const polyfillUrl = Cypress.env("polyfillUrl");
+    cy.request(polyfillUrl).then(response => {
+      polyfill = response.body;
+    });
+  });
+  it("should initialise the 3 Wrapping Key Custodians", () => {
+    cy.server();
+    cy
+      .route(
+        "post",
+        `${Cypress.env("api_server2")}/${orga_name}/onboarding/next`
+      )
+      .as("next");
+    cy
+      .route(
+        "post",
+        `${Cypress.env("api_server2")}/${orga_name}/onboarding/authenticate`
+      )
+      .as("authenticate");
+    cy
+      .route(
+        "get",
+        `${Cypress.env("api_server2")}/${orga_name}/onboarding/challenge`
+      )
+      .as("challenge");
+    cy.visit(Cypress.env("api_server"), {
+      onBeforeLoad: win => {
+        win.fetch = null;
+        win.eval(polyfill);
+        win.fetch = win.unfetch;
+      }
+    });
+
+    cy
+      .request("POST", Cypress.env("api_switch_device"), {
+        device_number: 1
+      })
+      .then(() => {
+        cy.get("input").type(orga_name);
+        cy.contains("continue").click();
+        cy.wait(1000);
+        cy.contains("Welcome").should("be.visible");
+        cy.contains("Get Started").click();
+        cy.wait("@next");
+        cy.contains("continue").click();
+        cy.wait("@next");
+        cy.contains("continue").click();
+        cy.wait("@next");
+        cy.contains("continue").click();
+        cy.wait("@next");
+        cy.wait("@challenge");
+        cy.get(":nth-child(1) > .fragment").click();
+        cy.wait("@authenticate");
+        cy.request("POST", Cypress.env("api_switch_device"), {
+          device_number: 2
+        });
+        cy.get(":nth-child(2) > .fragment").click();
+        cy.wait("@authenticate");
+        cy.request("POST", Cypress.env("api_switch_device"), {
+          device_number: 3
+        });
+        // Cancel the approval
+        cy.request("POST", Cypress.env("approve_cancel_device"), {
+          approve: false
+        });
+        cy.get(":nth-child(3) > .fragment").click();
+        cy.wait("@authenticate");
+
+        // Do the last WPK
+        cy.get(":nth-child(3) > .fragment").click();
+        cy.wait("@authenticate");
+        cy.contains("continue").click();
+        cy.wait("@next");
+        cy
+          .contains("continue")
+          .debug()
+          .click();
+        cy.wait("@next");
+        cy
+          .contains("continue")
+          .debug()
+          .click();
+        cy.wait("@next");
+      });
+  });
+});
---
+++ cypress/integration/Onboarding_Wipe/04_wipe_by_admin.spec.js
@@ -0,0 +1,84 @@
+const orga_name = Cypress.env("workspace");
+context("Admin Approve the registration of the Shared Owners", () => {
+  let polyfill;
+  before(() => {
+    const polyfillUrl = Cypress.env("polyfillUrl");
+    cy.request(polyfillUrl).then(response => {
+      polyfill = response.body;
+    });
+  });
+  it("should sign in with all the admin to approve the Shared Owners", () => {
+    cy.server();
+    cy
+      .route(
+        "post",
+        `${Cypress.env("api_server2")}/${orga_name}/onboarding/next`
+      )
+      .as("next");
+    cy
+      .route(
+        "post",
+        `${Cypress.env("api_server2")}/${orga_name}/onboarding/authenticate`
+      )
+      .as("authenticate");
+    cy
+      .route(
+        "get",
+        `${Cypress.env("api_server2")}/${orga_name}/onboarding/challenge`
+      )
+      .as("challenge");
+    cy.request("POST", Cypress.env("api_switch_device"), {
+      device_number: 4
+    });
+    cy
+      .visit(Cypress.env("api_server"), {
+        onBeforeLoad: win => {
+          win.fetch = null;
+          win.eval(polyfill);
+          win.fetch = win.unfetch;
+        }
+      })
+      .then(() => {
+        cy.get("input").type(orga_name);
+        cy.contains("continue").click();
+        cy.wait(1000);
+        cy.get(".test-onboarding-signin").click();
+        cy.wait("@authenticate");
+
+        cy.request("POST", Cypress.env("api_switch_device"), {
+          device_number: 5
+        });
+
+        // Cancel on the device
+        cy.request("POST", Cypress.env("approve_cancel_device"), {
+          approve: false
+        });
+        cy.get(".test-onboarding-signin").click();
+        cy
+          .contains("Shared-Owners registration confirmation")
+          .should("be.visible");
+        //  Choose the first option go back
+        cy
+          .contains("Oops! you clicked by mistake...")
+          .click()
+        cy.contains("Go back").click();
+
+        // choose to register shared owner again
+        cy.request("POST", Cypress.env("approve_cancel_device"), {
+          approve: false
+        });
+        cy.get(".test-onboarding-signin").click();
+        cy
+          .contains("You've made a mistake when registering ")
+          .click();
+        cy.contains("Register Shared-Owners again").click();
+        //
+        cy.contains("Prerequisites").should("be.visible");
+        cy.contains("continue").click();
+        cy.wait("@next");
+        cy.contains("continue").click();
+        cy.wait("@next");
+
+      });
+  });
+});
