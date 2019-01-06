ffa1e169fb5611fa3da8d6cffe323988b2b2ea2f
meriadec <meriadec.pillet@gmail.com>
2019-01-06 16:44:34 +0100

    Update erc20 import script & erc20 token list

--- scripts/fetch-erc20-list.sh
+++ scripts/fetch-erc20-list.sh
@@ -1,8 +1,9 @@
 #!/bin/env bash
+# shellcheck disable=SC2001
 
 # ------
-UPSTREAM=git@github.com:jchampemont/crypto-assets.git
-BRANCH=first-import
+UPSTREAM=git@github.com:LedgerHQ/crypto-assets.git
+BRANCH=master
 OUTPUT_FILE=src/data/erc20-list.json
 # ------
 
@@ -16,20 +17,26 @@ function main {
 
   echo "-- cloning from $UPSTREAM"
   git clone "$UPSTREAM" "$TMP_DIR"
-
-  # TODO: remove this once the token has been signed
-  echo "-- changing branch"
   cd "$TMP_DIR"
   git checkout "$BRANCH"
   cd -
 
   echo "-- concatenating all json files of $DIR_TO_SCAN"
   FULL=$(
-    while read -r f ; do echo -n "$(cat "$f"), " ; done \
-      < <(find "$TMP_DIR/$DIR_TO_SCAN" -name '*.json')
+    while read -r f ; do
+      DIR_NAME=$(dirname "$f")
+      OUTPUT="$(cat "$f"), "
+      SIG_FILE="$DIR_NAME/ledger_signature.json"
+      if [[ -e $SIG_FILE ]]; then
+        OUTPUT=$(echo "$OUTPUT" | sed "s/}/, \"signature\": $(cat "$SIG_FILE") }/g")
+      else
+        >&2 echo "Signature not found for $f"
+      fi
+      echo -n "$OUTPUT";
+    done < <(find "$TMP_DIR/$DIR_TO_SCAN" -name 'common.json')
   )
 
-  echo "[${FULL::-2}]" \
+  echo "[${FULL::-1}]" \
     | ./node_modules/.bin/prettier --stdin --parser json \
     | tee "$OUTPUT_FILE"
 
