891bfb1762d0b658236fa17a8faacf11c32a4279
Anastasia Poupeney <anastasiasergienko@gmail.com>
2019-01-17 12:55:37 +0100

    refactor copy to clipboard button in all relevant places

--- src/components/Send/04-Confirmation.js
+++ src/components/Send/04-Confirmation.js
@@ -1,18 +1,15 @@
 // @flow
 import React, { PureComponent, Fragment } from "react";
 import { withStyles } from "@material-ui/core/styles";
-import Button from "@material-ui/core/Button";
 import type { WalletBridge } from "bridge/types";
 import type { Account } from "data/types";
 import { Trans } from "react-i18next";
-import { CopyToClipboard } from "react-copy-to-clipboard";
 import LineRow from "components/LineRow";
 import AccountName from "components/AccountName";
 import OverviewOperation from "components/OverviewOperation";
 import Amount from "components/Amount";
 import DialogButton from "components/buttons/DialogButton";
-import Copy from "components/icons/Copy";
-import colors from "shared/colors";
+import CopyToClipboardButton from "components/CopyToClipboardButton";
 
 import InfoModal from "components/InfoModal";
 import SendLayout from "./SendLayout";
@@ -20,29 +17,6 @@ import SendLayout from "./SendLayout";
 const styles = {
   warningMsg: {
     marginTop: 25
-  },
-  recipientContainer: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    "&:hover $buttonCopy": {
-      display: "flex",
-      backgroundColor: colors.cream
-    }
-  },
-  recipient: {
-    overflow: "hidden",
-    textOverflow: "ellipsis"
-  },
-  buttonCopy: {
-    display: "none",
-    position: "fixed",
-    alignSelf: "flex-end",
-    color: colors.ocean,
-    fontSize: 11
-  },
-  copyIcon: {
-    marginRight: 3
   }
 };
 
@@ -55,14 +29,12 @@ type Props<Transaction> = {
 };
 
 type State = {
-  copied: boolean,
   fees: ?number,
   totalSpent: ?number
 };
 
 class SendConfirmation extends PureComponent<Props<*>, State> {
   state = {
-    copied: false,
     fees: null,
     totalSpent: null
   };
@@ -77,20 +49,9 @@ class SendConfirmation extends PureComponent<Props<*>, State> {
     this.setState({ fees, totalSpent });
   }
 
-  onCopy = () => {
-    this.setState({ copied: true });
-    this._timeout = setTimeout(() => this.setState({ copied: false }), 1e3);
-  };
-
-  componentWillUnmount() {
-    this._timeout && clearTimeout(this._timeout);
-  }
-
-  _timeout: ?TimeoutID = null;
-
   render() {
     const { account, bridge, transaction, classes, confirmTx } = this.props;
-    const { copied, fees, totalSpent } = this.state;
+    const { fees, totalSpent } = this.state;
 
     const amount = bridge.getTransactionAmount(account, transaction);
     const recipient = bridge.getTransactionRecipient(account, transaction);
@@ -108,33 +69,7 @@ class SendConfirmation extends PureComponent<Props<*>, State> {
             />
             <div>
               <LineRow label={<Trans i18nKey="send:confirmation.identifier" />}>
-                {recipient && (
-                  <div className={classes.recipientContainer}>
-                    <span className={classes.recipient}>{recipient}</span>
-                    <CopyToClipboard text={recipient} onCopy={this.onCopy}>
-                      <Button
-                        className={classes.buttonCopy}
-                        variant="contained"
-                        size="small"
-                      >
-                        {copied ? (
-                          <span>
-                            <Trans i18nKey="common:copied" />
-                          </span>
-                        ) : (
-                          <Fragment>
-                            <div className={classes.copyIcon}>
-                              <Copy color={colors.ocean} size={12} />
-                            </div>
-                            <span>
-                              <Trans i18nKey="common:copy" />
-                            </span>
-                          </Fragment>
-                        )}
-                      </Button>
-                    </CopyToClipboard>
-                  </div>
-                )}
+                {recipient && <CopyToClipboardButton textToCopy={recipient} />}
               </LineRow>
               <LineRow label={<Trans i18nKey="send:confirmation.account" />}>
                 <AccountName account={account} />
