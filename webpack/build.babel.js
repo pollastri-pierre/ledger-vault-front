21d958a13753982bcdab57b23a579c26e1c1e64a
meriadec <meriadec.pillet@gmail.com>
2019-01-22 12:23:17 +0100

    Upgrade webpack, add and configure storybook

--- webpack/build.babel.js
+++ webpack/build.babel.js
@@ -1,10 +1,11 @@
-import HappyPack from "happypack";
 import merge from "webpack-merge";
 import webpack from "webpack";
 
 import webpackConfig from "./base";
 
 export default merge(webpackConfig, {
+  mode: "production",
+
   output: {
     filename: "[name]-[chunkhash].js"
   },
@@ -13,44 +14,26 @@ export default merge(webpackConfig, {
     rules: [
       {
         test: /\.js$/,
-        use: "happypack/loader",
+        use: ["thread-loader", "babel-loader?sourceMap"],
         exclude: /node_modules/
       }
     ]
   },
 
-  plugins: [
-    new HappyPack({ loaders: ["babel-loader?sourceMap"], verbose: false }),
-
-    new webpack.optimize.CommonsChunkPlugin({
-      name: "vendor",
-      minChunks: m => /node_modules/.test(m.context)
-    }),
-
-    new webpack.optimize.CommonsChunkPlugin({
-      name: "manifest",
-      minChunks: Infinity
-    }),
-
-    new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        comparisons: true,
-        conditionals: true,
-        dead_code: true,
-        evaluate: true,
-        if_return: true,
-        join_vars: true,
-        screw_ie8: true,
-        sequences: true,
-        unused: true,
-        warnings: false
-      },
-      output: {
-        comments: false
+  optimization: {
+    minimize: true,
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendor",
+          chunks: "initial"
+        }
       }
-    })
-  ],
+    }
+  },
+
+  plugins: [new webpack.optimize.OccurrenceOrderPlugin()],
 
   stats: {
     colors: true,
