8ed5f02d8770c6a39734368c47d667b9df35105a
Florent Teissier <teissierflorent@gmail.com>
2019-02-14 11:10:46 +0100

    Introduce ModalHeader and ModalBody component
    + draft of group modal connected to mock-api

---
+++ src/components/base/Modal/ModalHeader.js
@@ -0,0 +1,26 @@
+// @flow
+
+import React from "react";
+import Box from "components/base/Box";
+import ModalClose from "components/base/Modal/ModalClose";
+import Text from "components/base/Text";
+
+export default ({
+  onClose,
+  title,
+  children
+}: {
+  onClose: () => void,
+  title?: string,
+  children?: React$Node
+}) => (
+  <Box horizontal align="center" justify="space-between" p={20}>
+    {children && children}
+    {title && (
+      <Text header bold>
+        {title}
+      </Text>
+    )}
+    <ModalClose onClick={onClose} />
+  </Box>
+);
