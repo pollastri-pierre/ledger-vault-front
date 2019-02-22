60b863889585ac1d62950f376798d1d0a6d35e79
Anastasia Poupeney <anastasiasergienko@gmail.com>
2019-02-22 09:38:55 +0100

    editable field for lineRow

---
+++ src/components/EditableField.js
@@ -0,0 +1,77 @@
+// @flow
+
+import React, { PureComponent, Fragment } from "react";
+import { FaRegEdit, FaCheck } from "react-icons/fa";
+import Text from "components/base/Text";
+import Box from "components/base/Box";
+import InputField from "components/InputField";
+
+import colors from "shared/colors";
+
+type Props = {
+  value: string,
+  onChange: string => void
+};
+type State = {
+  editMode: boolean,
+  localValue: string
+};
+const styles = {
+  cursor: {
+    cursor: "pointer"
+  }
+};
+class EditableField extends PureComponent<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      editMode: false,
+      localValue: props.value
+    };
+  }
+
+  handleChange = (value: string) => {
+    this.setState({ localValue: value });
+  };
+
+  toggleEditMode = () => {
+    const { editMode } = this.state;
+    this.setState({ editMode: !editMode });
+  };
+
+  onConfirm = () => {
+    const { onChange } = this.props;
+    const { localValue } = this.state;
+    onChange(localValue);
+    this.toggleEditMode();
+  };
+
+  render() {
+    const { editMode, localValue } = this.state;
+    const { value } = this.props;
+    return (
+      <Box horizontal align="center" flow={10}>
+        {!editMode ? (
+          <Fragment>
+            <Text>{value}</Text>
+            <FaRegEdit style={styles.cursor} onClick={this.toggleEditMode} />
+          </Fragment>
+        ) : (
+          <InputField
+            value={localValue}
+            onChange={this.handleChange}
+            renderRight={
+              <FaCheck
+                color={colors.green}
+                style={styles.cursor}
+                onClick={this.onConfirm}
+              />
+            }
+          />
+        )}
+      </Box>
+    );
+  }
+}
+
+export default EditableField;
---
+++ src/components/LineRow/stories.js
@@ -0,0 +1,61 @@
+/* eslint-disable react/prop-types */
+// @flow
+
+import React, { Component } from "react";
+
+import { storiesOf } from "@storybook/react";
+
+import { genMembers, genAccounts } from "data/mock-entities";
+import type { Account, Member } from "data/types";
+
+import LineRow from "components/LineRow";
+import AccountName from "components/AccountName";
+import Amount from "components/Amount";
+import DateFormat from "components/DateFormat";
+import Box from "components/base/Box";
+import EditableField from "components/EditableField";
+
+const members: Member[] = genMembers(10);
+const accounts: Account[] = genAccounts(3, { members });
+
+const account: Account = accounts[0];
+
+storiesOf("Components", module).add("LineRow", () => <Wrapper />);
+
+type State = {
+  username: string
+};
+class Wrapper extends Component<*, State> {
+  state = {
+    username: account.name
+  };
+
+  onEditUsername = (username: string) => {
+    this.setState({ username });
+  };
+
+  render() {
+    const { username } = this.state;
+    return (
+      <Box p={40}>
+        <LineRow label="Account">
+          <AccountName account={account} />
+        </LineRow>
+        <LineRow label="Balance">
+          <Amount
+            account={account}
+            value={account.balance}
+            strong
+            erc20Format={account.account_type === "ERC20"}
+          />
+        </LineRow>
+        <LineRow label="Date">
+          <DateFormat date={account.created_on} />
+        </LineRow>
+        <LineRow label="Editable Name">
+          <EditableField value={username} onChange={this.onEditUsername} />
+        </LineRow>
+      </Box>
+    );
+  }
+}
