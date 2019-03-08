c62e195e481d82d996b68b8076ff4becddff58f3
Florent Teissier <teissierflorent@gmail.com>
2019-03-08 16:30:40 +0100

    introduce UserContext to have `me` everywhere
    If you need `me` in your component, just decorate it with
    `withMe(YourComponent)` and you will get `me` for free in the props.

---
+++ src/components/UserContextProvider/index.js
@@ -0,0 +1,24 @@
+// @flow
+import React from "react";
+import connectData from "restlay/connectData";
+import ProfileQuery from "api/queries/ProfileQuery";
+import type { Member } from "data/types";
+
+export const UserContext: React$Context<?Member> = React.createContext(null);
+
+export const withMe = (Comp: React$ComponentType<*>) => (props: Object) => (
+  <UserContext.Consumer>
+    {me => <Comp {...props} me={me} />}
+  </UserContext.Consumer>
+);
+
+const UserContextProvider = ({ children, me }: { children: *, me: Member }) => (
+  <UserContext.Provider value={me}>{children}</UserContext.Provider>
+);
+
+// TODO render loading
+export default connectData(UserContextProvider, {
+  queries: {
+    me: ProfileQuery
+  }
+});
