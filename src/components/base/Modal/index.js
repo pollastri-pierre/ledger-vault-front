23afa1892ec6c296ba23c01ebcba0362986b12ce
meriadec <meriadec.pillet@gmail.com>
2019-03-08 16:20:23 +0100

    Fix horrible modal behaviour in Chrome 72

--- src/components/base/Modal/index.js
+++ src/components/base/Modal/index.js
@@ -32,6 +32,7 @@ const ModalDialogInner = styled(Box).attrs({ bg: "white" })`
   margin-bottom: 40px;
   margin-top: 40px;
   border-radius: 4px;
+  flex-shrink: 0;
   box-shadow: 0px 11px 15px -7px rgba(0, 0, 0, 0.04),
     0px 24px 38px 3px rgba(0, 0, 0, 0.04), 0px 9px 46px 8px rgba(0, 0, 0, 0.04);
 `;
