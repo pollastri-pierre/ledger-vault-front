480632d6d0892ed9e20d97e76719acb392b31753
Florent Teissier <teissierflorent@gmail.com>
2019-03-12 14:20:48 +0100

    Add <EntityStatus> on top of <Status>, wrapped by withMe()
    This component check if current user has already approved the
    request/entity so it can pass a different status to Status

---
+++ src/components/EntityStatus/index.js
@@ -0,0 +1,35 @@
+// @flow
+
+import React, { PureComponent } from "react";
+import Status from "components/Status";
+import { withMe } from "components/UserContextProvider";
+import type { Member, Request } from "data/types";
+import { hasUserApprovedRequest } from "utils/request";
+
+type Props = {
+  status: string,
+  request: ?Request,
+  me: Member
+};
+
+class EntityStatus extends PureComponent<Props> {
+  render() {
+    const { status, me, request } = this.props;
+    if (!request || !request.approvals) {
+      return <Status status={status} />;
+    }
+
+    const waitingForAppal =
+      !hasUserApprovedRequest(request, me) && status !== "ACTIVE";
+
+    return (
+      <Status
+        withWarning={waitingForAppal}
+        status={waitingForAppal ? "AWAITING_APPROVAL" : status}
+      />
+    );
+  }
+}
+
+export { EntityStatus };
+export default withMe(EntityStatus);
