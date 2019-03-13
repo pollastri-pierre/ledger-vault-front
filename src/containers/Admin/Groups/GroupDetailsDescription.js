7f19a09c04c3ad783776290b282cd6cb8b78beda
Florent Teissier <teissierflorent@gmail.com>
2019-03-13 17:21:15 +0100

    use GroupEditDesc mutation in group modal + improve style

--- src/containers/Admin/Groups/GroupDetailsDescription.js
+++ src/containers/Admin/Groups/GroupDetailsDescription.js
@@ -4,27 +4,34 @@ import Box from "components/base/Box";
 import type { Group } from "data/types";
 import EditableText from "components/base/EditableText";
 import React, { PureComponent } from "react";
+import connectData from "restlay/connectData";
+import type { RestlayEnvironment } from "restlay/connectData";
+import EditGroupDescriptionMutation from "api/mutations/EditGroupDescriptionMutation";
 
 type Props = {
-  group: Group
+  group: Group,
+  restlay: RestlayEnvironment
 };
 
 type State = {
   loading: boolean
 };
 
-const delay = ms => new Promise(success => setTimeout(success, ms));
-
 class GroupDetailsDescription extends PureComponent<Props, State> {
   state = {
     loading: false
   };
 
-  onChange = async () => {
-    console.warn("TODO: udpate description with a Query");
+  onChange = async (description: string) => {
     this.setState({ loading: true });
-    await delay(2000);
-    this.setState({ loading: false });
+    const { restlay, group } = this.props;
+    try {
+      await restlay.commitMutation(
+        new EditGroupDescriptionMutation({ group, description })
+      );
+    } finally {
+      this.setState({ loading: false });
+    }
   };
 
   render() {
@@ -36,10 +43,11 @@ class GroupDetailsDescription extends PureComponent<Props, State> {
           text={group.description || ""}
           onChange={this.onChange}
           loading={loading}
+          isEditDisabled={group.status !== "ACTIVE"}
         />
       </Box>
     );
   }
 }
 
-export default GroupDetailsDescription;
+export default connectData(GroupDetailsDescription);
