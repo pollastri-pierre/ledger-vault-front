0e42fca96c06cf1c719e4106b8b322ec9101f826
Florent Teissier <teissierflorent@gmail.com>
2019-03-13 17:18:13 +0100

    introduce EditGroupDescriptionMutation

---
+++ src/api/mutations/EditGroupDescriptionMutation.js
@@ -0,0 +1,33 @@
+// @flow
+import Mutation from "restlay/Mutation";
+import type { Group } from "data/types";
+import { success, error } from "formatters/notification";
+import schema from "data/schema";
+
+type In = {
+  group: Group,
+  description: string
+};
+
+type Res = Group;
+
+export default class EditGroupDescriptionMutation extends Mutation<In, Res> {
+  method = "PUT";
+
+  uri = `/groups/${this.props.group.id}`;
+
+  responseSchema = schema.Group;
+
+  getSuccessNotification() {
+    return success("group's description", "saved");
+  }
+
+  getErrorNotification(e: Error) {
+    return error("group's description", "saved", e);
+  }
+
+  getBody() {
+    const { description } = this.props;
+    return { description };
+  }
+}
