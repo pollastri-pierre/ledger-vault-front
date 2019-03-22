9ca7369ac7f999bbd46d2ab9f5f140af800d94e0
Anastasia Poupeney <anastasiasergienko@gmail.com>
2019-03-22 15:14:16 +0100

    cleaning legacy code

--- src/containers/index.js
+++ src/containers/index.js
@@ -3,7 +3,6 @@ export { default as SandBox } from "./SandBox/SandBox";
 export { default as PrivateRoute } from "./Login/PrivateRoute";
 export { default as Logout } from "./Login/Logout";
 export { default as AlertsContainer } from "./AlertsContainer";
-// export { default as PendingRequests } from "./PendingRequests/PendingRequests";
 export { default as AccountView } from "./Account/AccountView";
 
 // FIXME deprecated. directly import the file/folder component
