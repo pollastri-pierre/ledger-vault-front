3e294fa7544e27687816d25b5c14f353b9a51e75
meriadec <meriadec.pillet@gmail.com>
2019-03-22 11:34:52 +0100

    Simplify Modals container

--- src/containers/Modals/index.js
+++ src/containers/Modals/index.js
@@ -1,6 +1,5 @@
 // @flow
-import React, { PureComponent, Fragment } from "react";
-import type { Match } from "react-router-dom";
+import React, { Fragment } from "react";
 import ModalRoute from "components/ModalRoute";
 import CreateGroup from "containers/Admin/Groups/CreateGroup";
 import GroupDetails from "containers/Admin/Groups/GroupDetails";
@@ -8,29 +7,19 @@ import AccountCreationFlow from "components/AccountCreationFlow";
 import UserDetails from "containers/Admin/Users/UserDetails";
 import AccountDetails from "containers/Admin/Accounts/AccountDetails";
 
-type Props = {
-  match: Match,
-};
-
-class Modals extends PureComponent<Props> {
-  render() {
-    return (
-      <Fragment>
-        <ModalRoute path="*/groups/details/:groupId" render={GroupDetails} />
-        <ModalRoute path="*/users/details/:userID" component={UserDetails} />
-        <ModalRoute
-          path="*/accounts/details/:accountId"
-          component={AccountDetails}
-        />
-        <ModalRoute path="*/groups/new" render={CreateGroup} />
-        <ModalRoute
-          path="*/accounts/new"
-          component={AccountCreationFlow}
-          disableBackdropClick
-        />
-      </Fragment>
-    );
-  }
-}
-
-export default Modals;
+export default () => (
+  <Fragment>
+    <ModalRoute path="*/groups/details/:groupId" render={GroupDetails} />
+    <ModalRoute path="*/users/details/:userID" component={UserDetails} />
+    <ModalRoute
+      path="*/accounts/details/:accountId"
+      component={AccountDetails}
+    />
+    <ModalRoute path="*/groups/new" render={CreateGroup} />
+    <ModalRoute
+      path="*/accounts/new"
+      component={AccountCreationFlow}
+      disableBackdropClick
+    />
+  </Fragment>
+);
