e9cd587af6131891878d77ac996772f73b2ed9ab
Najla <najla.amdouni@ledger.fr>
2019-04-01 17:32:54 +0200

    changing order

--- cypress/integration/Drop2/03_approve_account.spec.js
+++ cypress/integration/Drop2/03_approve_account.spec.js
@@ -20,27 +20,32 @@ describe("Tests Creation Account", function() {
     route();
     // John Clark
     cy
-      .visit("https://localhost:9000/ledger1/register/3c1513bc-c4a2-41c0-a6af-da8e130e0a15");
+      .visit("https://localhost:9000/ledger1/register/e9bb230e-d6c0-4300-8f02-563423abf22e");
+    cy.contains("John Clark");
     cy.get("[data-test=dialog-button]").click();
 
     // James Lepic
     cy
-      .visit("https://localhost:9000/ledger1/register/6093e6fb-494f-427a-98f5-3c58c25470f4");
+      .visit("https://localhost:9000/ledger1/register/829cb650-ac13-4fb8-a00d-9c630f642e97");
+    cy.contains("James Lepic");
     cy.get("[data-test=dialog-button]").click();
 
     //Thomas Lebron
     cy
-      .visit("https://localhost:9000/ledger1/register/0a38bcd6-123d-4066-95b1-efc507819ff2");
+      .visit("https://localhost:9000/ledger1/register/d4ca750f-3f43-49a4-84cb-36f3f4fa0302");
+    cy.contains("Thomas Lebron");
     cy.get("[data-test=dialog-button]").click();
 
     //Aidan Fisher
     cy
-      .visit("https://localhost:9000/ledger1/register/063d93bd-d3e7-4dde-9f4f-f101b9468708");
+      .visit("https://localhost:9000/ledger1/register/18a1766f-2968-4d89-889c-4b09d05bf4ca");
+    cy.contains("Aidan Fisher");
     cy.get("[data-test=dialog-button]").click();
 
     //Anna Wagner
     cy
-      .visit("https://localhost:9000/ledger1/register/25d99a01-f972-4fed-834d-46b31d4e39f0");
+      .visit("https://localhost:9000/ledger1/register/4d4730fd-1c80-45c9-b732-431c26129562");
+    cy.contains("Anna Wagner");
     cy.get("[data-test=dialog-button]").click();
 
 
