babe7824ef76096704db040b7a91961cc19de2d1
meriadec <meriadec.pillet@gmail.com>
2019-04-17 11:05:37 +0200

    Update InputText stories

--- src/components/base/form/stories.js
+++ src/components/base/form/stories.js
@@ -5,14 +5,13 @@ import { storiesOf } from "@storybook/react";
 import { text } from "@storybook/addon-knobs";
 import { FaLink } from "react-icons/fa";
 
-import Box from "components/base/Box";
 import { InputText } from "components/base/form";
 
-const p = {
+const p = () => ({
   value: text("value", ""),
   placeholder: text("placeholder", "Enter some text..."),
   autoFocus: true,
-};
+});
 
 const errors = [new Error("Invalid value")];
 const warnings = [new Error("Can't verify your address")];
@@ -21,6 +20,11 @@ const hints = [
     key: "maxChars",
     label: v => `10 chars max (${10 - v.length} left)`,
     check: v => v !== "",
+  },
+  {
+    key: "whatever",
+    label: `Very long inspiration hint that should take multiple lines`,
+    check: () => true,
   },
   {
     key: "numbers",
@@ -34,30 +38,12 @@ const hints = [
   },
 ];
 
-const hintsBig = [
-  hints[0],
-  {
-    key: "whatever",
-    label: `Very long inspiration hint that should take multiple lines`,
-    check: () => true,
-  },
-  hints[1],
-];
-
 storiesOf("form/InputText", module)
   .addDecorator(story => <div style={{ maxWidth: 400 }}>{story()}</div>)
-  .add("basic", () => <InputText {...p} />)
-  .add("icon", () => <InputText {...p} IconLeft={FaLink} />)
-  .add("error", () => <InputText {...p} errors={errors} />)
-  .add("warning", () => <InputText {...p} warnings={warnings} />)
+  .add("basic", () => <InputText {...p()} />)
+  .add("icon", () => <InputText {...p()} IconLeft={FaLink} />)
+  .add("error", () => <InputText {...p()} errors={errors} />)
+  .add("warning", () => <InputText {...p()} warnings={warnings} />)
   .add("hints", () => (
-    <Box flow={40}>
-      <InputText {...p} value={text("value", "")} hints={hints} />
-      <InputText
-        {...p}
-        value={text("value", "40420.aa")}
-        autoFocus={false}
-        hints={hintsBig}
-      />
-    </Box>
+    <InputText autoFocus value={text("value", "40420.aa")} hints={hints} />
   ));
