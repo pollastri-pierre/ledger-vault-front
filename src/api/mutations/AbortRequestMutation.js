6c3a96da78a50d5f078df4ba3af85479e1787cb2
Florent Teissier <teissierflorent@gmail.com>
2019-04-29 10:24:19 +0200

    display feedback (error|sucess) on abort request

--- src/api/mutations/AbortRequestMutation.js
+++ src/api/mutations/AbortRequestMutation.js
@@ -1,6 +1,7 @@
 // @flow
 import Mutation from "restlay/Mutation";
 import type { Request } from "data/types";
+import { success, error } from "formatters/notification";
 
 type Input = {
   requestID: string,
@@ -12,6 +13,14 @@ export default class AbortRequestMutation extends Mutation<Input, Response> {
   method = "POST";
 
   uri = `/requests/${this.props.requestID}/abort`;
+
+  getSuccessNotification = () => {
+    return success("request", "aborted");
+  };
+
+  getErrorNotification = (e: Error) => {
+    return error("request", "aborted", e);
+  };
 
   getBody() {
     // temp body until it is removed from the gate
