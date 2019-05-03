cd0ebc2c98824be125e8e1da063a73fbfc41c4ba
meriadec <meriadec.pillet@gmail.com>
2019-05-03 15:31:37 +0200

    Ability to click on accounts is group accounts list

--- src/containers/Admin/Groups/GroupDetailsAccounts.js
+++ src/containers/Admin/Groups/GroupDetailsAccounts.js
@@ -1,6 +1,10 @@
 // @flow
+
 import React, { PureComponent } from "react";
+import styled from "styled-components";
+import ButtonBase from "@material-ui/core/ButtonBase";
 import { Trans } from "react-i18next";
+
 import type { Account } from "data/types";
 import connectData from "restlay/connectData";
 import CurrencyAccountValue from "components/CurrencyAccountValue";
@@ -10,10 +14,10 @@ import AccountName from "components/AccountName";
 import SpinnerCard from "components/spinners/SpinnerCard";
 import Text from "components/base/Text";
 import Box from "components/base/Box";
+import VaultLink from "components/VaultLink";
 
 type Props = {
   accounts: Account[],
-  // group: Group
 };
 
 class GroupDetailsAccounts extends PureComponent<Props> {
@@ -25,23 +29,47 @@ class GroupDetailsAccounts extends PureComponent<Props> {
       );
     }
     return (
-      <Box grow style={{ overflow: "auto" }} flow={20}>
+      <Container>
         {accounts.map(a => (
-          <Box key={a.id} noShrink flow={3}>
-            <AccountName account={a} />
-            <Text small color="grey">
-              <CurrencyAccountValue
-                account={a}
-                value={a.balance}
-                erc20Format={a.account_type === "ERC20"}
-              />
-            </Text>
-          </Box>
+          <VaultLink key={a.id} to={`/admin/accounts/view/${a.id}`}>
+            <AccountButton>
+              <Box flow={3}>
+                <AccountName account={a} />
+                <Text small color="grey">
+                  <CurrencyAccountValue
+                    account={a}
+                    value={a.balance}
+                    erc20Format={a.account_type === "ERC20"}
+                  />
+                </Text>
+              </Box>
+            </AccountButton>
+          </VaultLink>
         ))}
-      </Box>
+      </Container>
     );
   }
 }
+
+const AccountButton = styled(ButtonBase)`
+  && {
+    border: 1px solid #f0f0f0;
+    border-radius: 4px;
+    padding: 15px;
+  }
+`;
+
+const Container = styled(Box).attrs({
+  horizontal: true,
+})`
+  align: flex-start;
+  flex-wrap: wrap;
+
+  > * {
+    margin-right: 10px !important;
+    margin-bottom: 10px !important;
+  }
+`;
 
 const RenderLoading = () => <SpinnerCard />;
 
