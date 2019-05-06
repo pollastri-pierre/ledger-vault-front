c66f4ab791becf15c9ed60c4dd22e6e8a19b8195
Florent Teissier <teissierflorent@gmail.com>
2019-05-06 15:45:44 +0200

    display read only approvals step in account details

--- src/containers/Admin/Accounts/AccountTransactionRules.js
+++ src/containers/Admin/Accounts/AccountTransactionRules.js
@@ -1,11 +1,44 @@
 // @flow
 import React from "react";
-import RulesViewer from "components/ApprovalsRules/RulesViewer";
-import type { Account } from "data/types";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
-const AccountTransactionRules = ({ account }: { account: Account }) =>
+import connectData from "restlay/connectData";
+import UsersQuery from "api/queries/UsersQuery";
+import GroupsQuery from "api/queries/GroupsQuery";
+import ApprovalsRules from "components/ApprovalsRules";
+import Box from "components/base/Box";
+import { deserializeApprovalSteps } from "utils/accounts";
+import type { Account, User, Group } from "data/types";
+import type { Connection } from "restlay/ConnectionQuery";
+
+const AccountTransactionRules = ({
+  account,
+  users,
+  groups,
+}: {
+  account: Account,
+  users: Connection<User>,
+  groups: Connection<Group>,
+}) =>
   account.tx_approval_steps ? (
-    <RulesViewer rules={account.tx_approval_steps} />
+    <ApprovalsRules
+      rules={deserializeApprovalSteps(account.tx_approval_steps)}
+      users={users.edges.map(u => u.node)}
+      groups={groups.edges.map(g => g.node)}
+      readOnly
+      onChange={() => {}}
+    />
   ) : null;
 
-export default AccountTransactionRules;
+const RenderLoading = () => (
+  <Box align="center">
+    <CircularProgress size={20} />;
+  </Box>
+);
+export default connectData(AccountTransactionRules, {
+  RenderLoading,
+  queries: {
+    users: UsersQuery,
+    groups: GroupsQuery,
+  },
+});
