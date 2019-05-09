6f79207b435ca074a15fe217d6f547c444709acc
meriadec <meriadec.pillet@gmail.com>
2019-05-09 18:20:14 +0200

    Update wording & clean unused stuff

--- src/api/queries/ValidateAddressQuery.js
+++ src/api/queries/ValidateAddressQuery.js
@@ -16,7 +16,7 @@ const uri = ({ address, currency }: Input) =>
   `/validation/${currency.id}/${encodeURIComponent(address)}`;
 
 // Calculate the fee for a given account (in the account currency)
-// (used when creating a new operation)
+// (used when creating a new transaction)
 export default class ValidateAddressQuery extends Query<Input, Response> {
   uri = uri(this.props);
 }
--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -4,41 +4,7 @@
 
 import React from "react";
 import colors from "shared/colors";
-import Profile from "./thin/Profile";
-import PeopleThin from "./thin/People";
-import ValidateBadge from "./full/ValidateBadge";
-import RatesThin from "./thin/Rates";
 import Plug from "./thin/Plug";
-import HourglassThin from "./thin/Hourglass";
-import ClockThin from "./thin/Clock";
-
-export function SecurityMembersIcon() {
-  return (
-    <PeopleThin
-      style={{
-        width: "15px",
-      }}
-      color={colors.mouse}
-    />
-  );
-}
-
-export function SecurityQuorumIcon() {
-  return <ValidateBadge color={colors.mouse} />;
-}
-
-export function SecurityRateLimiterIcon() {
-  return <RatesThin style={{ width: "13px" }} color={colors.mouse} />;
-}
-
-export function SecurityTimelockIcon() {
-  return (
-    <HourglassThin
-      style={{ width: "12px", strokeWidth: "2px" }}
-      color={colors.mouse}
-    />
-  );
-}
 
 export function PlugIcon(props: *) {
   return (
@@ -51,35 +17,6 @@ export function PlugIcon(props: *) {
         strokeWidth: "2px",
       }}
       {...props}
-    />
-  );
-}
-
-export function BigSecurityTimeLockIcon() {
-  return (
-    <HourglassThin
-      style={{ width: "28px", height: "30px" }}
-      color={colors.mouse}
-    />
-  );
-}
-
-export function BigSecurityMembersIcon() {
-  return (
-    <Profile style={{ width: "26px", height: "30px" }} color={colors.mouse} />
-  );
-}
-
-export function BigSecurityRateLimiterIcon() {
-  return (
-    <RatesThin color={colors.mouse} style={{ width: "25px", height: "30px" }} />
-  );
-}
-export function BigSecurityAutoExpireIcon() {
-  return (
-    <ClockThin
-      color={colors.mouse}
-      className="security-icon security-autoexpire-big-icon"
     />
   );
 }
