e16db5424dd348efaca81c17f8f1335328f568a5
meriadec <meriadec.pillet@gmail.com>
2019-06-03 12:08:52 +0200

    Correct deserialize accounts in group

--- src/api/queries/AccountsInGroupQuery.js
+++ src/api/queries/AccountsInGroupQuery.js
@@ -2,6 +2,7 @@
 import Query from "restlay/Query";
 import schema from "data/schema";
 import type { Account } from "data/types";
+import { deserializeAccount } from "api/transformations/Account";
 
 type Input = {
   groupId: string,
@@ -13,4 +14,6 @@ export default class AccountsInGroupQuery extends Query<Input, Response> {
   uri = `/groups/${this.props.groupId}/accounts`;
 
   responseSchema = [schema.Account];
+
+  deserialize = deserializeAccount;
 }
