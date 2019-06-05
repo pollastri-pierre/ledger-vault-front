6fdc9314f294ac1f634c827c6a250f5dd44e0ac6
meriadec <meriadec.pillet@gmail.com>
2019-06-05 17:21:33 +0200

    Prevent click on edit account if there is pending transactions

--- src/containers/Admin/Accounts/AccountOverview.js
+++ src/containers/Admin/Accounts/AccountOverview.js
@@ -4,9 +4,28 @@ import { Trans } from "react-i18next";
 import LineRow from "components/LineRow";
 import CurrencyAccountValue from "components/CurrencyAccountValue";
 import AccountName from "components/AccountName";
+import Box from "components/base/Box";
+import InfoBox from "components/base/InfoBox";
 import type { Account } from "data/types";
 
-const AccountOverview = ({ account }: { account: Account }) => (
+const AccountOverview = ({
+  account,
+  hasPendingTransactions,
+}: {
+  account: Account,
+  hasPendingTransactions: boolean,
+}) => (
+  <Box flow={20}>
+    {hasPendingTransactions && (
+      <InfoBox type="info">
+        <Trans i18nKey="accountDetails:warnPendingTxs" />
+      </InfoBox>
+    )}
+    <Rows account={account} />
+  </Box>
+);
+
+const Rows = ({ account }: { account: Account }) => (
   <>
     <LineRow label={<Trans i18nKey="accountDetails:type" />}>
       {account.last_request && account.last_request.type}
