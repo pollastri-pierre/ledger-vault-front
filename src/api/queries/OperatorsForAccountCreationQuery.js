ca23bcde52eb95eb4ce5f3f4e106d4b380599707
Florent Teissier <teissierflorent@gmail.com>
2019-06-05 11:08:55 +0200

    remove admin from account tx rules in account creation

--- src/api/queries/OperatorsForAccountCreationQuery.js
+++ src/api/queries/OperatorsForAccountCreationQuery.js
@@ -12,8 +12,7 @@ type Node = User;
 const uri = () => {
   const finalQuery: Object = {
     status: "ACTIVE",
-    // FIXME FIXME FIXME remove ADMIN on develop-hsm2 branch
-    role: ["ADMIN", "OPERATOR"],
+    role: ["OPERATOR"],
   };
   const q = queryString.stringify(finalQuery);
   return `/people${q ? "?" : ""}${q}`;
