a908388ac9a8d62d6f9320bd36fe3c6da967bf22
meriadec <meriadec.pillet@gmail.com>
2019-06-05 16:03:47 +0200

    Minor unrelated polishes

--- src/api/transformations/Account.js
+++ src/api/transformations/Account.js
@@ -6,6 +6,10 @@ import type { Account } from "data/types";
 
 // flow don't even understand it was not an exact account.
 export function deserializeAccount(account: Account): Account {
+  if (!("balance" in account)) {
+    console.warn('No "balance" in account. Default to 0.');
+    account.balance = 0;
+  }
   return {
     ...account,
     balance: BigNumber(account.balance),
