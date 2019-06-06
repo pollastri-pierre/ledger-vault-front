07940ae3875c9c256873cd024badbbed0db8cc1b
meriadec <meriadec.pillet@gmail.com>
2019-06-06 11:44:53 +0200

    Fix isMemberOfFirst...

--- src/utils/users.js
+++ src/utils/users.js
@@ -4,4 +4,4 @@ import type { Account } from "data/types";
 
 // operators receive Null as a step if he is not part of it
 export const isMemberOfFirstApprovalStep = (account: Account) =>
-  account.tx_approval_steps && !account.tx_approval_steps[0];
+  !!account.tx_approval_steps && !!account.tx_approval_steps[0];
