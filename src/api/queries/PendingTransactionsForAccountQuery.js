26deac3c19e8ca97e763d82bec3897a77eb3092f
meriadec <meriadec.pillet@gmail.com>
2019-06-10 16:40:24 +0200

    Optimizations on account details

---
+++ src/api/queries/PendingTransactionsForAccountQuery.js
@@ -0,0 +1,19 @@
+// @flow
+import ConnectionQuery from "restlay/ConnectionQuery";
+import schema from "data/schema";
+import type { Transaction } from "data/types";
+
+type In = {
+  accountId: number,
+};
+
+type Node = Transaction;
+
+export default class PendingTransactionsForAccountQuery extends ConnectionQuery<
+  In,
+  Node,
+> {
+  uri = `/transactions?status=PENDING&accounts=${this.props.accountId}`;
+
+  nodeSchema = schema.Transaction;
+}
