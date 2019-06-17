6f0471b80dfd235e5a879cc2309e877536de7910
Florent Teissier <teissierflorent@gmail.com>
2019-06-17 11:06:25 +0200

    Introduce a Next button allowing to trigger /next on a request

---
+++ src/components/NextRequestButton.js
@@ -0,0 +1,51 @@
+// @flow
+import React, { PureComponent } from "react";
+
+import NextRequestMutation from "api/mutations/NextRequestMutation";
+import connectData from "restlay/connectData";
+import Button from "components/base/Button";
+import type { Request } from "data/types";
+import type { RestlayEnvironment } from "restlay/connectData";
+
+type Props = {
+  restlay: RestlayEnvironment,
+  request: Request,
+};
+
+type State = {
+  isLoading: boolean,
+};
+
+class NextRequestButton extends PureComponent<Props, State> {
+  state = {
+    isLoading: false,
+  };
+
+  triggerNext = async () => {
+    const { request, restlay } = this.props;
+    try {
+      this.setState({ isLoading: true });
+      await restlay.commitMutation(
+        new NextRequestMutation({ requestId: request.id }),
+      );
+    } finally {
+      this.setState({ isLoading: false });
+    }
+  };
+
+  render() {
+    const { isLoading } = this.state;
+    return (
+      <Button
+        variant="filled"
+        size="tiny"
+        onClick={this.triggerNext}
+        isLoading={isLoading}
+      >
+        RETRY
+      </Button>
+    );
+  }
+}
+
+export default connectData(NextRequestButton);
