17bb5810ef90d560bd52c877d2e40bd1e01268c6
Florent Teissier <teissierflorent@gmail.com>
2019-07-04 14:43:04 +0200

    move account tx approval step directly in overview

--- src/containers/Admin/Accounts/AccountOverview.js
+++ src/containers/Admin/Accounts/AccountOverview.js
@@ -6,6 +6,7 @@ import CurrencyAccountValue from "components/CurrencyAccountValue";
 import AccountName from "components/AccountName";
 import Box from "components/base/Box";
 import InfoBox from "components/base/InfoBox";
+import AccountTransactionRules from "containers/Admin/Accounts/AccountTransactionRules";
 import type { Account } from "data/types";
 
 const AccountOverview = ({
@@ -33,6 +34,14 @@ const Rows = ({ account }: { account: Account }) => (
     <LineRow label={<Trans i18nKey="accountDetails:balance" />}>
       <CurrencyAccountValue account={account} value={account.balance} />
     </LineRow>
+    {account.tx_approval_steps && (
+      <LineRow
+        vertical
+        label={<Trans i18nKey="entityModal:tabs.transactionRules" />}
+      >
+        <AccountTransactionRules account={account} />
+      </LineRow>
+    )}
   </div>
 );
 
