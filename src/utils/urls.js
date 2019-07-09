ff2f85cc8f00001cd34bc2bd4761e3c2d959e1bd
meriadec <meriadec.pillet@gmail.com>
2019-07-09 11:12:58 +0200

    Update the "update app" page

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,8 +1,8 @@
 // @flow
 
 export const urls = {
-  customer_support: "https://help.vault.ledger.com",
-  release_notes: "https://help.vault.ledger.com/Content/whatsnew.htm",
+  customer_support: "https://support.vault.ledger.com",
+  help_updater: "https://help.vault.ledger.com/operator",
   ledger_updater_mac:
     "https://github.com/LedgerHQ/ledger-updater/releases/download/v1.0.0/ledger-updater-1.0.0.dmg",
   ledger_updater_windows:
