71fca561c8ddedf7ca5d47f5c44ad8ebbf8bdc0d
meriadec <meriadec.pillet@gmail.com>
2019-08-29 22:32:33 +0200

    Add UpdateApp to Storybook

---
+++ src/components/UpdateApp/stories.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+
+import UpdateApp from "components/UpdateApp";
+
+storiesOf("components", module).add("UpdateApp", () => <UpdateApp />);
