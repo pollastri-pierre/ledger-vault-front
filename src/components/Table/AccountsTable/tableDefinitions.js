f72fc7734ea4a5306ebeaf5101777a6613ccddda
Anastasia Poupeney <anastasiasergienko@gmail.com>
2019-09-03 09:52:48 +0200

    fix operator account table

--- src/components/Table/AccountsTable/tableDefinitions.js
+++ src/components/Table/AccountsTable/tableDefinitions.js
@@ -48,7 +48,7 @@ export const accountsTableDefault = [
   },
   {
     header: {
-      label: "",
+      label: "Details",
       align: "right",
     },
     body: {
@@ -59,13 +59,69 @@ export const accountsTableDefault = [
   },
 ];
 
-export const roleColumn = {
-  header: {
-    label: <Trans i18nKey="accountView:permission" />,
-    align: "right",
+export const accountsIsOperatorTableDefault = [
+  {
+    header: {
+      label: "Account name",
+      align: "left",
+      sortable: true,
+    },
+    body: {
+      prop: "name",
+      align: "left",
+    },
   },
-  body: {
-    prop: "approver_role",
-    align: "right",
+  {
+    header: {
+      label: "Status",
+      align: "left",
+      sortable: true,
+    },
+    body: {
+      prop: "status",
+      align: "left",
+    },
   },
-};
+  {
+    header: {
+      label: "Countervalue",
+      align: "right",
+      sortable: false,
+    },
+    body: {
+      prop: "countervalue",
+      align: "right",
+    },
+  },
+  {
+    header: {
+      label: "Balance",
+      align: "right",
+    },
+    body: {
+      prop: "balance",
+      align: "right",
+    },
+  },
+  {
+    header: {
+      label: <Trans i18nKey="accountView:permission" />,
+      align: "right",
+    },
+    body: {
+      prop: "approver_role",
+      align: "right",
+    },
+  },
+  {
+    header: {
+      label: "Details",
+      align: "right",
+    },
+    body: {
+      prop: "details",
+      align: "center",
+      size: "small",
+    },
+  },
+];
