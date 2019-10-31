87db4009bb48a156d9b0117311b53661f5f29094
Anastasia Poupeney <anastasiasergienko@gmail.com>
2019-10-31 15:41:38 +0100

    remove hardcoded wording

--- src/containers/Admin/Accounts/AccountEditRequest.js
+++ src/containers/Admin/Accounts/AccountEditRequest.js
@@ -1,12 +1,12 @@
 // @flow
 
-import React, { PureComponent } from "react";
+import React from "react";
 
 import connectData from "restlay/connectData";
 import UsersQuery from "api/queries/UsersQuery";
 import GroupsQuery from "api/queries/GroupsQuery";
 import Box from "components/base/Box";
-import Spinner from "components/base/Spinner";
+import { SpinnerCentered } from "components/base/Spinner";
 import Text from "components/base/Text";
 import colors, { opacity } from "shared/colors";
 import RulesViewer from "components/ApprovalsRules/RulesViewer";
@@ -21,84 +21,80 @@ type Props = {
   groups: Connection<Group>,
 };
 
-class AccountEditRequest extends PureComponent<Props> {
-  render() {
-    const { account, groups, users } = this.props;
-    const { tx_approval_steps, last_request } = account;
+function AccountEditRequest(props: Props) {
+  const { account, groups, users } = props;
+  const { tx_approval_steps, last_request } = account;
 
-    if (!last_request) return null;
+  if (!last_request) return null;
 
-    const isAccountMigration = last_request.type === "MIGRATE_ACCOUNT";
-    const editData = last_request.edit_data || null;
+  const isAccountMigration = last_request.type === "MIGRATE_ACCOUNT";
+  const editData = last_request.edit_data || null;
 
-    const newRules = isAccountMigration
-      ? tx_approval_steps
-      : editData
-      ? resolveRules(
-          editData.governance_rules.tx_approval_steps,
-          groups.edges.map(e => e.node),
-          users.edges.map(e => e.node),
-        )
-      : null;
+  const newRules = isAccountMigration
+    ? tx_approval_steps
+    : editData
+    ? resolveRules(
+        editData.governance_rules.tx_approval_steps,
+        groups.edges.map(e => e.node),
+        users.edges.map(e => e.node),
+      )
+    : null;
 
-    const oldRules = isAccountMigration ? null : tx_approval_steps;
-    const hasNameChanged = editData && account.name !== editData.name;
+  const oldRules = isAccountMigration ? null : tx_approval_steps;
+  const hasNameChanged = editData && account.name !== editData.name;
 
-    const haveRulesChanged =
-      newRules && oldRules && haveRulesChangedDiff(newRules, oldRules);
-    return (
-      <Box flow={10} horizontal justify="space-between">
-        <Box bg={opacity(colors.grenade, 0.05)} {...diffBoxProps}>
-          <Box mb={20}>
-            <Text
-              size="small"
-              uppercase
-              fontWeight="bold"
-              color={opacity(colors.grenade, 0.8)}
-            >
-              BEFORE
-            </Text>
-          </Box>
-          {hasNameChanged && (
-            <Box mb={20}>
-              <b>Name</b>
-              <span>{account.name}</span>
-            </Box>
-          )}
-          {haveRulesChanged && (
-            <Box mb={20}>
-              <b>Rules</b>
-              <RulesViewer rules={oldRules} />
-            </Box>
-          )}
+  const haveRulesChanged =
+    newRules && oldRules && haveRulesChangedDiff(newRules, oldRules);
+  return (
+    <Box flow={10} horizontal justify="space-between">
+      <Box bg={opacity(colors.grenade, 0.05)} {...diffBoxProps}>
+        <Box mb={20}>
+          <Text
+            size="small"
+            uppercase
+            fontWeight="bold"
+            color={opacity(colors.grenade, 0.8)}
+            i18nKey="entityModal:diff.before"
+          />
         </Box>
-        <Box bg={opacity(colors.ocean, 0.05)} {...diffBoxProps}>
+        {hasNameChanged && (
           <Box mb={20}>
-            <Text
-              size="small"
-              uppercase
-              fontWeight="bold"
-              color={opacity(colors.ocean, 0.8)}
-            >
-              After
-            </Text>
+            <Text fontWeight="bold" i18nKey="entityModal:diff.name" />
+            <Text>{account.name}</Text>
           </Box>
-          {hasNameChanged && editData && (
-            <Box mb={20}>
-              <b>Name</b>
-              <span>{editData.name}</span>
-            </Box>
-          )}
-          {haveRulesChanged && (
-            <Box mb={20}>
-              <b>Rules</b>
-              <RulesViewer rules={newRules} />
-            </Box>
-          )}
-        </Box>
+        )}
+        {haveRulesChanged && (
+          <Box mb={20}>
+            <Text fontWeight="bold" i18nKey="entityModal:diff.rules" />
+            <RulesViewer rules={oldRules} />
+          </Box>
+        )}
       </Box>
-    );
-  }
+      <Box bg={opacity(colors.ocean, 0.05)} {...diffBoxProps}>
+        <Box mb={20}>
+          <Text
+            size="small"
+            uppercase
+            fontWeight="bold"
+            color={opacity(colors.ocean, 0.8)}
+            i18nKey="entityModal:diff.after"
+          />
+        </Box>
+        {hasNameChanged && editData && (
+          <Box mb={20}>
+            <Text fontWeight="bold" i18nKey="entityModal:diff.name" />
+            <Text>{editData.name}</Text>
+          </Box>
+        )}
+        {haveRulesChanged && (
+          <Box mb={20}>
+            <Text fontWeight="bold" i18nKey="entityModal:diff.rules" />
+            <RulesViewer rules={newRules} />
+          </Box>
+        )}
+      </Box>
+    </Box>
+  );
 }
 
 const diffBoxProps = {
@@ -107,17 +103,10 @@ const diffBoxProps = {
   flex: 1,
 };
 
-const RenderLoading = () => (
-  <Box align="center">
-    <Spinner />
-  </Box>
-);
-const C: React$ComponentType<$Shape<Props>> = connectData(AccountEditRequest, {
-  RenderLoading,
+export default connectData(AccountEditRequest, {
+  RenderLoading: () => <SpinnerCentered />,
   queries: {
     users: UsersQuery,
     groups: GroupsQuery,
   },
 });
-
-export default C;
