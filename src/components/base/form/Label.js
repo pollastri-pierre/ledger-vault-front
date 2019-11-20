86c926bd9ae741025d2fb708b998010682e7d96f
meriadec <meriadec.pillet@gmail.com>
2019-11-20 15:17:44 +0100

    Handle Infinity in threshold `max`

--- src/components/base/form/Label.js
+++ src/components/base/form/Label.js
@@ -8,7 +8,7 @@ import Box from "components/base/Box";
 export default styled(Box)`
   font-size: 11px;
   text-transform: uppercase;
-  padding-bottom: ${p => (p.noPadding ? 0 : 10)}px;
+  padding-bottom: ${p => (p.noPadding ? 0 : 5)}px;
   white-space: no-wrap;
   color: ${colors.textLight};
 `;
