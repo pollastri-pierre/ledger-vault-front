e88165b0c43025da0ebfc8b4962441aa9c7f90ab
Anastasia Poupeney <anastasiasergienko@gmail.com>
2019-11-26 14:38:45 +0100

    filter whitelists by currency in the addresses

--- src/components/AccountCreationFlow/steps/AccountCreationRules.js
+++ src/components/AccountCreationFlow/steps/AccountCreationRules.js
@@ -8,15 +8,22 @@ import type { AccountCreationStepProps } from "../types";
 export default (props: AccountCreationStepProps) => {
   const { payload, updatePayload, users, groups, whitelists } = props;
   const handleChangeRules = rulesSets => updatePayload({ rulesSets });
-
   const usersArray = users.edges.map(u => u.node);
   const groupsArray = groups.edges.map(g => g.node);
-  // TODO pass by a query param when gate is ready
-  const whitelistsArray = whitelists.edges
-    .map(w => w.node)
-    .filter(w => w.status === "ACTIVE");
 
   const { currency, erc20token } = payload;
+
+  const filteredWhitelistArray = whitelists.edges
+    .map(w => w.node)
+    .filter(w => {
+      const currencyId = currency ? currency.id : "ethereum";
+      return (
+        w.status === "ACTIVE" &&
+        w.addresses.find(a => {
+          return a.currency === currencyId;
+        })
+      );
+    });
 
   return (
     <MultiRules
@@ -24,7 +31,7 @@ export default (props: AccountCreationStepProps) => {
       onChange={handleChangeRules}
       users={usersArray}
       groups={groupsArray}
-      whitelists={whitelistsArray}
+      whitelists={filteredWhitelistArray}
       // $FlowFixMe
       currencyOrToken={erc20token || currency}
     />
