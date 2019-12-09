0cb10cc4a315f3f789c6686078d02dbb481303cd
Florent Teissier <teissierflorent@gmail.com>
2019-12-09 15:16:41 +0100

    display whitelist diff in 2 columns of added/removed addresses

--- src/containers/Admin/Whitelists/WhitelistEditRequest.js
+++ src/containers/Admin/Whitelists/WhitelistEditRequest.js
@@ -1,26 +1,28 @@
 // @flow
 
-import React, { Children, useState } from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
+import { MdClose, MdAdd } from "react-icons/md";
 import Box from "components/base/Box";
 import Button from "components/base/Button";
 import Text from "components/base/Text";
 import type { Whitelist, Address } from "data/types";
-import type { HistoryType } from "components/Address";
 import AddressComponent from "components/Address";
 import DiffName from "components/DiffName";
+import colors, { opacity } from "shared/colors";
 
 type Props = {
   whitelist: Whitelist,
-  hideUnchanged: boolean,
 };
 
 type AddressRow = {
-  history?: HistoryType,
   address: Address,
 };
 
+const NB_ITEM = 5;
 const WhitelistEditRequest = (props: Props) => {
-  const { whitelist, hideUnchanged } = props;
+  const [showMore, setShowMore] = useState(false);
+  const { whitelist } = props;
   if (!whitelist.last_request) return null;
   const { last_request } = whitelist;
   if (!last_request.edit_data) return null;
@@ -33,7 +35,7 @@ const WhitelistEditRequest = (props: Props) => {
   if (edit_data.addresses) {
     edit_data.addresses.forEach(a => {
       if (!findAddressInList(a, whitelist.addresses)) {
-        added.push({ history: "ADD", address: a });
+        added.push({ address: a });
       } else {
         unchanged.push({ address: a });
       }
@@ -41,80 +43,73 @@ const WhitelistEditRequest = (props: Props) => {
 
     whitelist.addresses.forEach(a => {
       if (!findAddressInList(a, edit_data.addresses)) {
-        removed.push({ history: "REMOVE", address: a });
+        removed.push({ address: a });
       } else {
         unchanged.push({ address: a });
       }
     });
   }
 
-  const unchangedWithoutDups = unchanged.filter(
-    (w, i, arr) =>
-      arr.findIndex(
-        el =>
-          el.address.name === w.address.name &&
-          el.address.currency === w.address.currency &&
-          el.address.address === w.address.address,
-      ) === i,
-  );
-  const all = [
-    ...removed,
-    ...added,
-    ...(hideUnchanged ? [] : unchangedWithoutDups),
-  ];
+  const hasAddressesChanged = added.length > 0 || removed.length > 0;
 
   return (
     <Box flow={20}>
       <DiffName entity={whitelist} />
-      {unchangedWithoutDups.length !== whitelist.addresses.length && (
-        <Box flow={15}>
-          <Text fontWeight="bold">Addresses</Text>
-          <Box flow={10}>
-            <div style={grid}>
-              <ShowMore number={8}>
-                {all.map(a => (
-                  <AddressComponent {...a} />
-                ))}
-              </ShowMore>
-            </div>
-          </Box>
-        </Box>
-      )}
+      <Box flow={15}>
+        {hasAddressesChanged && (
+          <>
+            <Text fontWeight="bold">Addresses</Text>
+            <Box flow={20}>
+              <Box flow={10} horizontal align="flex-start">
+                {added.length > 0 && (
+                  <Overlay type="add">
+                    <Box horizontal align="center" flow={5}>
+                      <MdAdd size={15} color={colors.green} />
+                      <Text>Addresses added</Text>
+                    </Box>
+                    {added
+                      .slice(0, showMore ? added.length : NB_ITEM)
+                      .map(a => (
+                        <AddressComponent {...a} />
+                      ))}
+                  </Overlay>
+                )}
+                {removed.length > 0 && (
+                  <Overlay>
+                    <Box horizontal align="center" flow={5}>
+                      <MdClose size={15} color={colors.grenade} />
+                      <Text>Addresses removed</Text>
+                    </Box>
+                    {removed
+                      .slice(0, showMore ? removed.length : NB_ITEM)
+                      .map(a => (
+                        <AddressComponent {...a} />
+                      ))}
+                  </Overlay>
+                )}
+              </Box>
+              {(NB_ITEM < added.length || NB_ITEM < removed.length) && (
+                <Button type="link" onClick={() => setShowMore(!showMore)}>
+                  {showMore ? "Show less" : "Show more"}
+                </Button>
+              )}
+            </Box>
+          </>
+        )}
+      </Box>
     </Box>
   );
 };
-const grid = {
-  display: "grid",
-  gridGap: 10,
-  gridTemplateColumns: "repeat(auto-fill, minmax(250px, 1fr) )",
-};
 
-const ShowMore = ({
-  children,
-  number,
-}: {
-  children: React$Node,
-  number: number,
-}) => {
-  const [showMore, setSetShowMore] = useState(false);
-  const childs = Children.toArray(children);
-
-  const showButton = number < childs.length;
-  return (
-    <>
-      {childs.slice(0, showButton ? childs.length : number)}
-      {showButton && (
-        <Button
-          onClick={() => setSetShowMore(!showMore)}
-          type="outline"
-          variant="primary"
-        >
-          {showMore ? "show less" : "show more"}
-        </Button>
-      )}
-    </>
-  );
-};
+const Overlay = styled(Box).attrs({ flow: 10 })`
+  padding: 20px;
+  flex: 1;
+  border-radius: 4px;
+  background: ${p =>
+    p.type === "add"
+      ? opacity(colors.green, 0.05)
+      : opacity(colors.grenade, 0.05)};
+`;
 
 export default WhitelistEditRequest;
 
