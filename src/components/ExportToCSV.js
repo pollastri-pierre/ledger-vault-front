4a871c766cf43b38fab35d5e35a56b8f43cffebb
meriadec <meriadec.pillet@gmail.com>
2020-01-03 11:39:03 +0100

    LV-2163 Improve UX of CSV export

--- src/components/ExportToCSV.js
+++ src/components/ExportToCSV.js
@@ -5,45 +5,36 @@ import moment from "moment";
 import network from "network";
 import omit from "lodash/omit";
 import { useTranslation } from "react-i18next";
-import styled from "styled-components";
-import { FaDownload, FaCheck } from "react-icons/fa";
+import { FaDownload } from "react-icons/fa";
 import type { ObjectParameters } from "query-string";
-
-import colors, { opacity } from "shared/colors";
 
 import TriggerErrorNotification from "components/TriggerErrorNotification";
 import Box from "components/base/Box";
-import Button from "components/base/Button";
-import Modal, {
-  RichModalHeader,
-  ModalBody,
-  ModalFooter,
-} from "components/base/Modal";
 import FakeLink from "components/base/FakeLink";
 
 const configByEntityType = {
   transaction: {
     endpoint: "/transactions",
     columns: [
-      { name: "operationType", isEnabled: true },
-      { name: "transactionNumber", isEnabled: true },
-      { name: "status", isEnabled: true },
-      { name: "family", isEnabled: true },
-      { name: "parentName", isEnabled: true },
-      { name: "currency", isEnabled: true },
-      { name: "accountName", isEnabled: true },
-      { name: "amount", isEnabled: true },
-      { name: "amountUnit", isEnabled: true },
-      { name: "fees", isEnabled: true },
-      { name: "feesUnit", isEnabled: true },
-      { name: "gasPrice", isEnabled: true },
-      { name: "gasLimit", isEnabled: true },
-      { name: "totalAmount", isEnabled: true },
-      { name: "hash", isEnabled: true },
-      { name: "sender", isEnabled: true },
-      { name: "recipient", isEnabled: true },
-      { name: "label", isEnabled: true },
-      { name: "note", isEnabled: true },
+      "operationType",
+      "transactionNumber",
+      "status",
+      "family",
+      "parentName",
+      "currency",
+      "accountName",
+      "amount",
+      "amountUnit",
+      "fees",
+      "feesUnit",
+      "gasPrice",
+      "gasLimit",
+      "totalAmount",
+      "hash",
+      "sender",
+      "recipient",
+      "label",
+      "note",
     ],
   },
 };
@@ -58,25 +49,20 @@ type Props = {
 export default function ExportToCSV(props: Props) {
   const { queryParams, entityType } = props;
   const { t } = useTranslation();
-  const [isModalOpened, setModalOpened] = useState(false);
   const [error, setError] = useState<?Error>(null);
 
   const config = configByEntityType[entityType];
 
-  const [columns, setColumns] = useState(config.columns);
-
-  const openModal = () => setModalOpened(true);
-  const closeModal = () => setModalOpened(false);
-
   const onExport = async () => {
     const params = omit(queryParams, ["pageSize"]);
-    const cols = columns.filter(c => c.isEnabled).map(c => c.name);
-    const labels = cols.map(col => t(`exportCSV:columns.${entityType}:${col}`));
+    const labels = config.columns.map(col =>
+      t(`exportCSV:columns.${entityType}:${col}`),
+    );
     try {
       const res = await network(
         `${config.endpoint}/export`,
         "POST",
-        { columns: cols, params, labels },
+        { columns: config.columns, params, labels },
         undefined,
         { noParse: true },
       );
@@ -93,91 +79,15 @@ export default function ExportToCSV(props: Props) {
     }
   };
 
-  const toggleColumn = c => () =>
-    setColumns(
-      columns.map(col => {
-        if (c.name === col.name) return { ...c, isEnabled: !c.isEnabled };
-        return col;
-      }),
-    );
-
   return (
     <>
       {error && <TriggerErrorNotification error={error} />}
-      <FakeLink onClick={openModal}>
+      <FakeLink onClick={onExport}>
         <Box horizontal align="center" flow={5}>
           <FaDownload />
           <span>Export to CSV</span>
         </Box>
       </FakeLink>
-      <Modal isOpened={isModalOpened} onClose={closeModal}>
-        <RichModalHeader
-          onClose={closeModal}
-          Icon={FaDownload}
-          title="Export to CSV"
-        />
-        <ModalBody width={500} flow={20}>
-          <div>Choose the columns you want to export:</div>
-          <ColumnsList>
-            {columns.map(c => (
-              <Column
-                key={c.name}
-                isEnabled={c.isEnabled}
-                onClick={toggleColumn(c)}
-              >
-                {t(`exportCSV:columns.${entityType}:${c.name}`)}
-              </Column>
-            ))}
-          </ColumnsList>
-        </ModalBody>
-        <ModalFooter pb={20} mr={10}>
-          <Button
-            type="filled"
-            variant="primary"
-            data-test="export-to-csv"
-            onClick={onExport}
-          >
-            Download
-          </Button>
-        </ModalFooter>
-      </Modal>
     </>
   );
 }
-
-const ColumnsList = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  margin: -5px;
-`;
-
-const StyledColumn = styled.div`
-  border-radius: 4px;
-  padding: 5px;
-  margin: 5px;
-  display: flex;
-  align-items: center;
-  background: ${p =>
-    p.isEnabled ? opacity(p.theme.colors.bLive, 0.1) : p.theme.colors.form.bg};
-  color: ${p => (p.isEnabled ? p.theme.colors.blue : "inherit")};
-  cursor: pointer;
-  &:hover {
-    opacity: 0.8;
-  }
-`;
-
-type ColumnProps = {
-  onClick: () => void,
-  children: React$Node,
-  isEnabled: boolean,
-};
-
-const Column = ({ children, isEnabled, onClick }: ColumnProps) => {
-  const color = isEnabled ? opacity(colors.blue, 0.3) : "rgba(0, 0, 0, 0.1)";
-  return (
-    <StyledColumn isEnabled={isEnabled} onClick={onClick}>
-      <FaCheck color={color} />
-      <span style={{ marginLeft: 5 }}>{children}</span>
-    </StyledColumn>
-  );
-};
