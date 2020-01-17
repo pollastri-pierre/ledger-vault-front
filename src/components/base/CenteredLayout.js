ce9334845f7affaecfd78cb04ca309aa4a8d5981
meriadec <meriadec.pillet@gmail.com>
2020-01-17 17:18:17 +0100

    Make CenteredLayout not leak outside of screen

--- src/components/base/CenteredLayout.js
+++ src/components/base/CenteredLayout.js
@@ -1,19 +1,44 @@
 // @flow
 
+import React from "react";
 import styled from "styled-components";
-
-import Box from "components/base/Box";
 
 import colors from "shared/colors";
 
-export default styled(Box).attrs({
-  align: "center",
-  justify: "center",
-})`
+const ScrollingBlock = styled.div`
   position: fixed;
   top: 0;
   right: 0;
   left: 0;
   bottom: 0;
-  background-color: ${p => p.bg || colors.cream};
+  background-color: ${colors.cream};
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  overflow: auto;
 `;
+
+const Outer = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-shrink: 1;
+  min-height: 0;
+`;
+
+const Inner = styled.div`
+  margin-top: 80px;
+  margin-bottom: 80px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export default ({ children }: { children: React$Node }) => (
+  <ScrollingBlock>
+    <Outer>
+      <Inner>{children}</Inner>
+    </Outer>
+  </ScrollingBlock>
+);
