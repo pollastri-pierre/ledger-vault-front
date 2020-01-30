4a744c4f0de727c174c058328a6e4b68b5243463
meriadec <meriadec.pillet@gmail.com>
2020-01-30 16:40:38 +0100

    VFE-46 Add notion of "product version", whatever that means

--- src/components/VersionLink/index.js
+++ src/components/VersionLink/index.js
@@ -3,11 +3,12 @@
 import React from "react";
 
 import Text from "components/base/Text";
+import productVersion from "../../version";
 
 const VersionLink = ({ onClick }: { onClick: boolean => void }) => {
   return (
     <span onClick={onClick} style={{ cursor: "pointer" }}>
-      <Text size="small">Vault - v{VAULT_FRONT_VERSION}</Text>
+      <Text size="small">Ledger Vault - {productVersion}</Text>
     </span>
   );
 };
