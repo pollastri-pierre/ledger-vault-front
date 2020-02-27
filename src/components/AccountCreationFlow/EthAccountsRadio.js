c2aae09e057ce48119a1ffe3fd647ebdd3890859
Paul Luong <luongpaul95@gmail.com>
2020-02-27 18:22:02 +0100

    VFE-106_rewrite-radio

--- src/components/AccountCreationFlow/EthAccountsRadio.js
+++ src/components/AccountCreationFlow/EthAccountsRadio.js
@@ -1,9 +1,9 @@
 // @flow
 
 import React from "react";
-import Radio from "@material-ui/core/Radio";
 import { Trans } from "react-i18next";
 
+import { Radio } from "components/base/form";
 import SelectAccount from "components/SelectAccount";
 import Text from "components/base/Text";
 import Box from "components/base/Box";
@@ -26,15 +26,16 @@ export default function EthAccountsRadio({
   };
 
   return (
-    <>
+    <Box flow={20}>
       <Box
         cursor="pointer"
         horizontal
         align="flex-start"
         onClick={selectFirstIfNotSet}
+        flow={15}
       >
-        <Radio color="primary" checked={account !== null} />
-        <Box my={15}>
+        <Radio checked={account !== null} />
+        <Box style={{ paddingTop: 2 }}>
           <Trans i18nKey="newAccount:erc20.selectExisting" />
           <Box mt={10} width={380}>
             <SelectAccount
@@ -45,10 +46,16 @@ export default function EthAccountsRadio({
           </Box>
         </Box>
       </Box>
-      <Box cursor="pointer" horizontal align="center" onClick={onChooseNull}>
-        <Radio color="primary" checked={account === null} />
-        <Text i18nKey="newAccount:erc20.createNew" />
+      <Box
+        cursor="pointer"
+        horizontal
+        align="flex-start"
+        onClick={onChooseNull}
+        flow={15}
+      >
+        <Radio checked={account === null} />
+        <Text style={{ paddingTop: 2 }} i18nKey="newAccount:erc20.createNew" />
       </Box>
-    </>
+    </Box>
   );
 }
--- src/components/base/form/index.js
+++ src/components/base/form/index.js
@@ -9,3 +9,4 @@ export { default as Label } from "./Label";
 export { default as TextArea } from "./inputs/TextArea";
 export { default as Switch } from "./Switch";
 export { default as Checkbox } from "./Checkbox";
+export { default as Radio } from "./Radio";
