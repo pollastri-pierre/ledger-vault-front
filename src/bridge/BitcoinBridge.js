c3a30c5a4f0e9dcf55323dd08ea2ed15d6ba4bb7
meriadec <meriadec.pillet@gmail.com>
2020-04-15 15:39:57 +0200

    Revert "VFE-81 Allow transaction to SegWit address"
    This reverts commit 67f7575c45aedc33f2beea9c013bf6616252d71a.

--- src/bridge/BitcoinBridge.js
+++ src/bridge/BitcoinBridge.js
@@ -5,7 +5,7 @@ import ValidateAddressQuery from "api/queries/ValidateAddressQuery";
 import type { Speed } from "api/queries/AccountCalculateFeeQuery";
 import type { Account, TransactionCreationNote } from "data/types";
 import FeesBitcoinKind from "components/FeesField/BitcoinKind";
-import { InvalidAddress } from "utils/errors";
+import { InvalidAddress, AddressShouldNotBeSegwit } from "utils/errors";
 import ExtraFieldBitcoinKind from "components/ExtraFields/BitcoinKind";
 import type { UtxoPickingStrategy } from "utils/utxo";
 import type { WalletBridge } from "./types";
@@ -43,7 +43,10 @@ const isRecipientValid = async (restlay, currency, recipient) => {
 const getRecipientError = async (restlay, currency, recipient) => {
   const isValid = await isRecipientValid(restlay, currency, recipient);
   if (!isValid) return new InvalidAddress(null, { ticker: currency.ticker });
-  return null;
+
+  const isSegwit = isAddressSegwit(currency, recipient);
+
+  return isSegwit ? new AddressShouldNotBeSegwit() : null;
 };
 
 const BitcoinBridge: WalletBridge<Transaction> = {
@@ -128,5 +131,12 @@ const BitcoinBridge: WalletBridge<Transaction> = {
   },
   getRecipientError,
 };
+
+function isAddressSegwit(currency, recipient) {
+  if (currency.id === "bitcoin" || currency.id === "bitcoin_testnet") {
+    return recipient.startsWith("bc1");
+  }
+  return false;
+}
 
 export default BitcoinBridge;
